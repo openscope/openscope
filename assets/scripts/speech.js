f50046b30a7438d6cdabd1f2007e412d8b2633e0
Jon Ross <jonrosszlsa@gmail.com>
2014-12-22 10:55:44 -0800

    add speech synthesis toggle

--- assets/scripts/speech.js
+++ assets/scripts/speech.js
@@ -1,9 +1,30 @@
 
 var speech = window.speechSynthesis;
+var speech_enabled = false;
 
-function sayText (textToSay) {
-  if (speech != null) {
-    textToSay = textToSay.replace(/[0-9]/g,"$& "); //Split numbers into individual digits e.g. Speedbird 666 -> Speedbird 6 6 6
+function speech_init() {
+  if('atc-speech-enabled' in localStorage && localStorage['atc-speech-enabled'] == 'true')
+    speech_enabled = true;
+}
+
+function sayText(textToSay) {
+  if(speech != null && speech_enabled) {
+    // Split numbers into individual digits e.g. Speedbird 666 -> Speedbird 6 6 6
+    textToSay = textToSay.replace(/[0-9]/g,"$& ").replace(/0/g, "zero");
     speech.speak(new SpeechSynthesisUtterance(textToSay));
   }
+}
+
+function speech_toggle() {
+  speech_enabled = !speech_enabled;
+
+  if(speech_enabled) {
+    $(".speech-toggle").addClass("active");
+  } else {
+    $(".speech-toggle").removeClass("active");
+    speech.cancel();
+  }
+
+  localStorage['atc-speech-enabled'] = speech_enabled;
+
 }
