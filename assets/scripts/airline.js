22a008efa3832ad324a8f8fd4b679102301180f3
David Albertson <albertson.d@gmail.com>
2015-04-23 08:42:21 -0700

    Revert "Rework EHAM and added KLM, KLC"

--- assets/scripts/airline.js
+++ assets/scripts/airline.js
@@ -11,8 +11,6 @@ function airline_init() {
   airline_load("AWE");
   airline_load("CESSNA");
   airline_load("ACA");
-  airline_load("KLM");
-  airline_load("KLC");
 }
 
 function airline_load(icao) {
