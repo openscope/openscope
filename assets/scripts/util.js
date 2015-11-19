5304b504e5f42236cddafcc26b6454315cd9452d
Dmitri Lebedev <ryba4@ryba4.com>
2015-11-19 15:45:04 +0600

    simplified code a bit

--- assets/scripts/util.js
+++ assets/scripts/util.js
@@ -435,16 +435,20 @@ function point_in_poly(point, vs) {
     // ray-casting algorithm based on
     // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
     
-    var x = point[0], y = point[1];
-    
-    var inside = false;
-    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+    var x = point[0],
+        y = point[1],
+        i,
+        j = vs.length - 1,
+        inside = false;
+
+    for (i in vs) {
         var xi = vs[i][0], yi = vs[i][1];
         var xj = vs[j][0], yj = vs[j][1];
         
         var intersect = ((yi > y) != (yj > y))
             && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
         if (intersect) inside = !inside;
+        j = i;
     }
     
     return inside;
