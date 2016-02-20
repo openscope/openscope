09ccfe6343ec2b61e3c9be96eeca444754856955
Maicom M. Rodeghiero <maicom_mr@hotmail.com>
2016-02-20 04:19:32 -0200

    Final Fixes
    Fixed Scripts(I miss it), and UAE problem.

--- assets/scripts/airline.js
+++ assets/scripts/airline.js
@@ -134,58 +134,71 @@ function airline_init() {
 	  airline_load("PAA");
 	  airline_load("TWA");
 	  
-  // Civil Airlines
-  airline_load("UAL");
-  airline_load("BAW");
+  // North America
   airline_load("AAL");
-  airline_load("AWE");
-  airline_load("CESSNA");
   airline_load("ACA");
-  airline_load("BCY");
-  airline_load("KLM");
-  airline_load("KLC");
-  airline_load("DAL");
   airline_load("AIRTAXI");
-  airline_load("LIGHTGA");
-  airline_load("FASTGA");
-  airline_load("SAS");
-  airline_load("NAX");
-  airline_load("EIN");
-  airline_load("RYR");
-  airline_load("BER");
-  airline_load("EZY");
-  airline_load("GWI");
+  airline_load("AMX");
   airline_load("AUA");
-
+  airline_load("AWE");
+  airline_load("BAW");
+  airline_load("BCY");
+  airline_load("BER");
+  airline_load("CESSNA");
+  airline_load("DAL");
+  airline_load("EIN");
+  airline_load("EZY");
+  airline_load("FASTGA");
+  airline_load("GWI");
+  airline_load("KLC");
+  airline_load("LIGHTGA");
+  airline_load("NAX");
+  airline_load("RYR");
+  airline_load("SAS");
+  airline_load("THY");
+  airline_load("UAL");
+  airline_load("VIR");
+  
+  //Europe
+	airline_load("AEA");
+	airline_load("AFR");
+	airline_load("AZA");
+	airline_load("IBE");
+    airline_load("DLH");
+    airline_load("KLM");
+  
   // South American
-  airline_load("ONE");
-  airline_load("GLO");
-  airline_load("TAM");
+  airline_load("ACA");
+  airline_load("ARG");
   airline_load("AVA");
   airline_load("AZU");
   airline_load("EMBRAER");
-  airline_load("ARG");
+  airline_load("GLO");
+  airline_load("LAN");
+  airline_load("ONE");
+  airline_load("TAM");
 
   // Asian
   airline_load("AFL");
-  airline_load("MOV");
-  airline_load("RLU");
-  airline_load("SBI");
-  airline_load("SVR");
-  airline_load("TYA");
   airline_load("ASA");
-  airline_load("SWA");
-  
-  // Asian
+  airline_load("CCA");
   airline_load("CES");
   airline_load("CPA");
   airline_load("CSN");
   airline_load("HDA");
+  airline_load("KAL");
+  airline_load("MOV");
+  airline_load("RLU");
+  airline_load("SBI");
+  airline_load("SVR");
+  airline_load("SWA");
+  airline_load("TYA");
+  airline_load("UAE");
 
   // Cargo Airlines
+  airline_load("CWC");
   airline_load("FDX");
   airline_load("UPS");
-  airline_load("CWC");
 
   // Military Air Forces
   airline_load("RFF");
@@ -193,25 +206,6 @@ function airline_init() {
   airline_load("USAF");
 
   // Internationals to South America
-  airline_load("SA-AEA");
-  airline_load("SA-AFR");
-  airline_load("SA-AMX");
-  airline_load("SA-AVA");
-  airline_load("SA-AZA");
-  airline_load("SA-BAW");
-  airline_load("SA-CCA");
-  airline_load("SA-DLH");
-  airline_load("SA-IBE");
-  airline_load("SA-KAL");
-  airline_load("SA-KLM");
-  airline_load("SA-LAN");
-  airline_load("SA-UAE");
-  airline_load("SA-UAL");
-
-  airline_load("DLH");
-  airline_load("VIR");
-  airline_load("THY");
-
 }
 
 function airline_load(icao) {
