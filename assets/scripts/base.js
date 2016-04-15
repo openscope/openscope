9a111cc8780316cedbacc91e0ad62dd2bdddacf6
Erik Quinn <erikquinn2@gmail.com>
2016-04-15 11:26:02 -0500

    Add class 'Area'

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -131,3 +131,33 @@ var Position=Fiber.extend(function() {
     },
   };
 });
+
+/** An enclosed region defined by a series of Position objects and an altitude range
+ ** @param {array} poly - series of Position objects that outline the shape
+ **                Note: DO NOT repeat the origin to 'close' the shape. Unnecessary.
+ ** @param {number} floor - (optional) altitude of bottom of area, in hundreds of feet
+ ** @param {number} ceiling - (optional) altitude of top of area, in hundreds of feet
+ ** @param {string} airspace_class - (optional) FAA airspace classification (A,B,C,D,E,G)
+ */
+var Area = Fiber.extend(function() {
+  return {
+    init: function(positions, /*optional*/ floor, ceiling, airspace_class) {
+      if(!positions) return;
+      this.poly     = [];
+      this.floor    = null;
+      this.ceiling  = null;
+      this.airspace_class = null;
+
+      if(floor != null) this.floor = floor;
+      if(ceiling != null) this.ceiling = ceiling;
+      if(airspace_class) this.airspace_class = airspace_class;
+
+      this.parse(positions);
+    },
+    parse: function(positions) {
+      for(var i=0; i<positions.length; i++) this.poly.push(positions[i]);
+      if(this.poly[0] == this.poly[this.poly.length-1])
+        this.poly.pop();  // shape shouldn't fully close; will draw with 'cc.closepath()'
+    }
+  };
+});
