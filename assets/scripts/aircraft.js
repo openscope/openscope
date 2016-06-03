06348c4e0b88b1972ecfa0d3124e80beaa50f622
Erik Quinn <erikquinn2@gmail.com>
2016-06-03 15:13:34 -0500

    Fix issue with spawning on fix line far from boundary

--- assets/scripts/aircraft.js
+++ assets/scripts/aircraft.js
@@ -3399,7 +3399,7 @@ function aircraft_update() {
       remove = true;
     }
     // Clean up the screen from aircraft that are too far
-    if((!aircraft_visible(aircraft,2) && !aircraft.inside_ctr) && !aircraft.fms.following.star){
+    if((!aircraft_visible(aircraft,2) && !aircraft.inside_ctr) && aircraft.fms.currentWaypoint().navmode == 'heading'){
       if(aircraft.category == "arrival") {
         remove = true;
       }
