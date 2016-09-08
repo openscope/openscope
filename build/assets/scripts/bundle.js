e7852fb680bfc66d0015d037d02845a55376e013
Nate Geslin <teamtomkins23@gmail.com>
2016-09-08 17:23:14 -0500

    Pulls in tools changes from feature/clean-the-window branch

--- build/assets/scripts/bundle.js
+++ build/assets/scripts/bundle.js
@@ -16836,9 +16836,9 @@ function load_module(name) {
   var filename;
   if (name[0] == "-") {
     modules[name].library = true;
-    filename = "assets/scripts/"+name.substr(1)+".js";
+    filename = "build/assets/scripts/"+name.substr(1)+".js";
   } else {
-    filename = "assets/scripts/"+name+".js";
+    filename = "build/assets/scripts/"+name+".js";
   }
   var el = document.createElement("script");
   el.src = filename;
