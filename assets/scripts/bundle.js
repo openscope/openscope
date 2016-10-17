f7b30b5e47fb55a86a4a1c1ac2a73669c936f898
Nate Geslin <teamtomkins23@gmail.com>
2016-10-16 22:13:34 -0500

    Updates bundle script

--- assets/scripts/bundle.js
+++ assets/scripts/bundle.js
@@ -42090,14 +42090,15 @@ Object.defineProperty(exports, "__esModule", {
 /**
  * List of airports to load.
  *
- *    {
- *      icao: {AIRPORT_ICAO},
- *      level: {AIRPORT_DIFFICULTY},
- *      name: {AIRPORT_NAME}
- *    }
- *
- *    If the airport is a work in progress, please include this icon after the `name` (see kmsp or ksea):
- *    - name: {AIRPORT_NAME} &#9983
+ * {
+ *   icao: {AIRPORT_ICAO},
+ *   level: {AIRPORT_DIFFICULTY},
+ *   name: {AIRPORT_NAME}
+ * }
+ * 
+ * If the airport is a work in progress, please include the icon character `&#9983` after the
+ * `name` (see kmsp or ksea):
+ * - name: {AIRPORT_NAME} &#9983
  *
  * @property AIRPORT_LOAD_LIST
  * @type {Array}
