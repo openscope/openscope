83c24992a02bc272ca8be83d5e8c3a4861b9df59
Nate Geslin <teamtomkins23@gmail.com>
2016-10-30 11:43:39 -0500

    feature/ATC-50 - adds gulp server task, removes http-server pkg and npm task

--- tools/tasks/utilityTasks.js
+++ tools/tasks/utilityTasks.js
@@ -1,50 +1,65 @@
 /* eslint-disable */
 'use strict';
 
-module.exports = function(gulp, config) {
-    var OPTIONS = config;
-    var rimraf = require('rimraf');
+module.exports = (gulp, config) => {
+    const OPTIONS = config;
+    const rimraf = require('rimraf');
+
+    ////////////////////////////////////////////////////////////////////
+    // SPAWN A CONNECT SERVER
+    ////////////////////////////////////////////////////////////////////
+    gulp.task('server', () => {
+        const connect = require('gulp-connect');
+        const path = require('path');
+
+        connect.server({
+            root: OPTIONS.DIR.BUILD,
+            port: 3003,
+            fallback: path.join(OPTIONS.DIR.BUILD, 'index.html')
+        });
+    });
+
 
     ////////////////////////////////////////////////////////////////////
     // CLEAN DESTINATION SCRIPT FILES
     ////////////////////////////////////////////////////////////////////
-    gulp.task('clean:build:scripts', function(cb) {
+    gulp.task('clean:build:scripts', (cb) => {
         rimraf(OPTIONS.DIR.BUILD_SCRIPTS, cb);
-    })
+    });
 
     ////////////////////////////////////////////////////////////////////
     // CLEAN DESTINATION STYLE FILES
     ////////////////////////////////////////////////////////////////////
-    gulp.task('clean:build:styles', function(cb) {
+    gulp.task('clean:build:styles', (cb) => {
         rimraf(OPTIONS.DIR.BUILD_STYLE, cb);
     });
 
     ////////////////////////////////////////////////////////////////////
     // CLEAN DESTINATION IMAGES
     ////////////////////////////////////////////////////////////////////
-    gulp.task('clean:build:images', function(cb) {
+    gulp.task('clean:build:images', (cb) => {
         rimraf(OPTIONS.DIR.BUILD_IMAGES, cb);
     });
 
     ////////////////////////////////////////////////////////////////////
     // CLEAN DESTINATION FONT FILES
     ////////////////////////////////////////////////////////////////////
-    gulp.task('clean:build:fonts', function(cb) {
+    gulp.task('clean:build:fonts', (cb) => {
         rimraf(OPTIONS.DIR.BUILD_FONTS, cb);
     });
 
     ////////////////////////////////////////////////////////////////////
     // CLEAN JSON DATA FOLDERS
     ////////////////////////////////////////////////////////////////////
-    gulp.task('clean:aircraft', function(cb) {
+    gulp.task('clean:aircraft', (cb) => {
         rimraf(OPTIONS.DIR.BUILD_AIRCRAFT, cb);
     });
 
-    gulp.task('clean:airlines', function(cb) {
+    gulp.task('clean:airlines', (cb) => {
         rimraf(OPTIONS.DIR.BUILD_AIRLINES, cb);
     });
 
-    gulp.task('clean:airports', function(cb) {
+    gulp.task('clean:airports', (cb) => {
         rimraf(OPTIONS.DIR.BUILD_AIRPORTS, cb);
     });
 
