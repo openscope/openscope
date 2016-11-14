f6ed2e3d1e05a620a3072138f8614043c9ff0c63
Nate Geslin <teamtomkins23@gmail.com>
2016-11-14 13:06:27 -0600

    bugfix/ATC-91 - Adds logic in FixCollection.init() that checks for current items, then destroys the current collection if items are present. * Removes area constant in AirportModel and adjusts .buildAirportAirspace() logic to use ._map() correctly

--- test/airport/fix/FixCollection.spec.js
+++ test/airport/fix/FixCollection.spec.js
@@ -3,10 +3,13 @@ import ava from 'ava';
 import FixCollection from '../../../src/assets/scripts/airport/Fix/FixCollection';
 import FixModel from '../../../src/assets/scripts/airport/Fix/FixModel';
 import { airportPositionFixture } from '../../fixtures/airportFixtures';
-import { FIX_LIST_MOCK } from './_mocks/fixMocks';
+import {
+    FIX_LIST_MOCK,
+    SMALL_FIX_LIST_MOCK
+} from './_mocks/fixMocks';
 
 
-ava.serial('FixCollection throws when an attempt to instantiate is made', t => {
+ava.serial('FixCollection throws when an attempt to instantiate is made with invalid params', t => {
     t.throws(() => new FixCollection());
 
     t.true(FixCollection._id === '');
@@ -43,4 +46,13 @@ ava.serial('.findRealFixes() returns a list of fixes that dont have `_` prepedni
     const result = FixCollection.findRealFixes();
 
     t.true(result.length === 104);
+});
+
+ava.serial('.init() resets _items when it is called with an existing collection', t => {
+    t.true(FixCollection.length === 105);
+
+    FixCollection.init(SMALL_FIX_LIST_MOCK);
+
+    t.false(FixCollection.length === 105);
+    t.true(FixCollection.length === 2);
 });
