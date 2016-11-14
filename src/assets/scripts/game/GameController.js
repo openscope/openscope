1d589ad863b1a500b69fbec4b18b322bdd1ca64b
Erik Quinn <erikquinn2@gmail.com>
2016-11-14 18:28:55 -0500

    Define game events and update score methods

--- src/assets/scripts/game/GameController.js
+++ src/assets/scripts/game/GameController.js
@@ -1,12 +1,54 @@
 /* eslint-disable camelcase, no-underscore-dangle, no-mixed-operators, func-names, object-shorthand,
 no-undef, class-methods-use-this */
 import $ from 'jquery';
+import _forEach from 'lodash/forEach';
+import _has from 'lodash/has';
 import GameOptions from './GameOptions';
 import { round } from '../math/core';
 import { SELECTORS } from '../constants/selectors';
+import { TIME } from '../constants/globalConstants';
 
 // Temporary const declaration here to attach to the window AND use as internal property
 const game = {};
+
+// TODO: Remember to move me to wherever the constants end up being moved to
+/**
+ * Definitions of point values for given game events
+ * @type {Object}
+ */
+const GAME_EVENTS_POINT_VALUES = {
+    AIRSPACE_BUST: -200,
+    ARRIVAL: 10,
+    COLLISION: -1000,
+    DEPARTURE: 10,
+    EXTREME_CROSSWIND_OPERATION: -15,
+    EXTREME_TAILWIND_OPERATION: -75,
+    GO_AROUND: -50,
+    HIGH_CROSSWIND_OPERATION: -5,
+    HIGH_TAILWIND_OPERATION: -25,
+    ILLEGAL_APPROACH_CLEARANCE: -10,
+    NOT_CLEARED_ON_ROUTE: -25,
+    SEPARATION_LOSS: -200
+};
+
+/**
+ * List of game events
+ * @type {Object}
+ */
+export const GAME_EVENTS = {
+    AIRSPACE_BUST: 'AIRSPACE_BUST',
+    ARRIVAL: 'ARRIVAL',
+    COLLISION: 'COLLISION',
+    DEPARTURE: 'DEPARTURE',
+    EXTREME_CROSSWIND_OPERATION: 'EXTREME_CROSSWIND_OPERATION',
+    EXTREME_TAILWIND_OPERATION: 'EXTREME_TAILWIND_OPERATION',
+    GO_AROUND: 'GO_AROUND',
+    HIGH_CROSSWIND_OPERATION: 'HIGH_CROSSWIND_OPERATION',
+    HIGH_TAILWIND_OPERATION: 'HIGH_TAILWIND_OPERATION',
+    ILLEGAL_APPROACH_CLEARANCE: 'ILLEGAL_APPROACH_CLEARANCE',
+    NOT_CLEARED_ON_ROUTE: 'NOT_CLEARED_ON_ROUTE',
+    SEPARATION_LOSS: 'SEPARATION_LOSS'
+};
 
 /**
  * @class GameController
@@ -17,7 +59,6 @@ export default class GameController {
      */
     constructor(getDeltaTime) {
         this.getDeltaTime = getDeltaTime;
-
         this.game = game;
         this.game.paused = true;
         this.game.focused = true;
@@ -25,40 +66,10 @@ export default class GameController {
         this.game.frequency = 1;
         this.game.time = 0;
         this.game.delta = 0;
+        this.game.events = {};
         this.game.timeouts = [];
-
-        // TODO: move to `setupHandlers()`
-        $(window).blur(() => {
-            this.game.focused = false;
-        });
-
-        $(window).focus(() => {
-            this.game.focused = true;
-        });
-
         this.game.last_score = 0;
-        this.game.score = {
-            arrival: 0,
-            departure: 0,
-
-            windy_landing: 0,
-            windy_takeoff: 0,
-
-            failed_arrival: 0,
-            failed_departure: 0,
-
-            warning: 0,
-            hit: 0,
-
-            abort: {
-                landing: 0,
-                taxi: 0
-            },
-
-            violation: 0,
-            restrictions: 0
-        };
-
+        this.game.score = 0;
         this.game.option = new GameOptions();
     }
 
@@ -68,76 +79,55 @@ export default class GameController {
      */
     init_pre() {
         prop.game = game;
-        // TODO: with the assignment of `prop.game = game`, the rest of these assignments are redundant and can
-        // be removed after proper testing
-        prop.game.paused = true;
-        prop.game.focused = true;
-        prop.game.speedup = 1;
-        prop.game.frequency = 1;
-        prop.game.time = 0;
-        prop.game.delta = 0;
-        prop.game.timeouts = [];
-
-        $(window).blur(() => {
-            prop.game.focused = false;
-        });
-
-        $(window).focus(() => {
-            prop.game.focused = true;
-        });
-
-        prop.game.last_score = 0;
-        prop.game.score = {
-            arrival: 0,
-            departure: 0,
-
-            windy_landing: 0,
-            windy_takeoff: 0,
-
-            failed_arrival: 0,
-            failed_departure: 0,
-
-            warning: 0,
-            hit: 0,
-
-            abort: {
-                landing: 0,
-                taxi: 0
-            },
-
-            violation: 0,
-            restrictions: 0
-        };
-
-        prop.game.option = new GameOptions();
+        this.game_initializeBlurFunctions();
+        this.events_initializeEventCount();
     }
 
     /**
-     * @for GameController
-     * @method game_get_score
-     */
-    game_get_score() {
-        let score = 0;
+    * Initialize `GameController.events` to contain appropriate properties with values of 0
+    * @for GameController
+    * @method events_initializeEventCount
+    * @return
+    */
+    events_initializeEventCount() {
+        _forEach(GAME_EVENTS, (gameEvent, key) => {
+            this.game.events[key] = 0;
+        });
+    }
 
-        score += prop.game.score.arrival * 10;
-        score += prop.game.score.departure * 10;
+    /**
+    * Record a game event to this.game.events, and update this.game.score
+    * @for GameController
+    * @method events_recordNew
+    * @param gameEvent {String} one of the events listed in GAME_EVENTS
+    */
+    events_recordNew(gameEvent) {
+        if (!_has(GAME_EVENTS, gameEvent)) {
+            throw new TypeError(`Expected a game event listed in GAME_EVENTS, but instead received ${gameEvent}`);
+        }
 
-        score -= prop.game.score.windy_landing * 0.5;
-        score -= prop.game.score.windy_takeoff * 0.5;
+        this.game.events[gameEvent] += 1;
+        this.game.score += GAME_EVENTS_POINT_VALUES[gameEvent];
+    }
 
-        score -= prop.game.score.failed_arrival * 20;
-        score -= prop.game.score.failed_departure * 2;
+    /**
+    * Initialize blur functions used during game pausing
+    * @for GameController
+    * @method game_initializeBlurFunctions
+    * @return
+    */
+    game_initializeBlurFunctions() {
+        // Set blurring function
+        $(window).blur(() => {
+            this.game.focused = false;
+            prop.game.focused = false;
+        });
 
-        score -= prop.game.score.warning * 5;
-        score -= prop.game.score.hit * 50;
-
-        score -= prop.game.score.abort.landing * 5;
-        score -= prop.game.score.abort.taxi * 2;
-
-        score -= prop.game.score.violation;
-        score -= prop.game.score.restrictions * 10;
-
-        return score;
+        // Set un-blurring function
+        $(window).focus(() => {
+            this.game.focused = true;
+            prop.game.focused = true;
+        });
     }
 
     /**
@@ -145,30 +135,24 @@ export default class GameController {
      * @method game_get_weighted_score
      */
     game_get_weighted_score() {
-        let score = this.game_get_score();
+        const hoursPlayed = this.game_time() / TIME.ONE_HOUR_IN_SECONDS;
+        const scorePerHour = this.game.score / hoursPlayed;
 
-        score = score / (this.game_time() / 60);
-        score *= 500;
-
-        return score;
+        return scorePerHour;
     }
 
     /**
      * @for GameController
-     * @method game_reset_score
+     * @method game_reset_score_and_events
      */
-    game_reset_score() {
-        prop.game.score.abort = { landing: 0, taxi: 0 };
-        prop.game.score.arrival = 0;
-        prop.game.score.departure = 0;
-        prop.game.score.failed_arrival = 0;
-        prop.game.score.failed_departure = 0;
-        prop.game.score.hit = 0;
-        prop.game.score.restrictions = 0;
-        prop.game.score.violation = 0;
-        prop.game.score.warning = 0;
-        prop.game.score.windy_landing = 0;
-        prop.game.score.windy_takeoff = 0;
+    game_reset_score_and_events() {
+        // Reset events
+        _forEach(this.game.events, (gameEvent, key) => {
+            this.game.events[key] = 0;
+        });
+
+        // Reset score
+        this.game.score = 0;
     }
 
     /**
@@ -317,10 +301,10 @@ export default class GameController {
 
     /**
      * @for GameController
-     * @method updateScore
+     * @method game_updateScore
      * @param score {number}
      */
-    updateScore(score) {
+    game_updateScore(score) {
         const $score = $(SELECTORS.DOM_SELECTORS.SCORE);
         $score.text(round(score));
 
@@ -338,10 +322,8 @@ export default class GameController {
      * @method update_pre
      */
     update_pre() {
-        const score = this.game_get_score();
-
-        if (score !== prop.game.last_score) {
-            this.updateScore(score);
+        if (this.game.score !== prop.game.last_score) {
+            this.game_updateScore(this.game.score);
         }
 
         prop.game.delta = Math.min(this.getDeltaTime() * prop.game.speedup, 100);
@@ -356,7 +338,7 @@ export default class GameController {
 
         for (let i = prop.game.timeouts.length - 1; i >= 0; i--) {
             let remove = false;
-            let timeout = prop.game.timeouts[i];
+            const timeout = prop.game.timeouts[i];
 
             if (this.game_time() > timeout[1]) {
                 timeout[0].call(timeout[5], timeout[2]);
