34d807cad0cbb6460fa99f7888df57b4ea0705af
Abraham Anderson <abeis@msn.com>
2017-02-25 22:28:54 -0600

    added tests for default parameter

--- test/base/BaseModel.spec.js
+++ test/base/BaseModel.spec.js
@@ -4,6 +4,20 @@ import _isString from 'lodash/isString';
 import BaseModel from '../../src/assets/scripts/client/base/BaseModel';
 import ExtendedBaseModelFixture from './_fixtures/ExtendedBaseModelFixture';
 
+ava('sets the #_id when passed invalid parameters', (t) => {
+    const modelojb = new BaseModel({});
+    t.true(modelojb._id.indexOf('BaseModel') !== -1);
+
+    const modelArray = new BaseModel([]);
+    t.true(modelArray._id.indexOf('BaseModel') !== -1);
+
+    const modelInt = new BaseModel(42);
+    t.true(modelInt._id.indexOf('BaseModel') !== -1);
+
+    const modelBool = new BaseModel(false);
+    t.true(modelBool._id.indexOf('BaseModel') !== -1);
+});
+
 ava('makes sure the model in instantiates with any data type', t => {
     t.notThrows(() => new BaseModel());
     t.notThrows(() => new BaseModel([]));
