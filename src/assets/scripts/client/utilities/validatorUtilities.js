8c7d4a8c5b210ca995fcebee5c8957e3f496f556
Abraham Anderson <abeis@msn.com>
2017-04-01 20:41:05 -0500

    created validatorUtilities and test file

---
+++ src/assets/scripts/client/utilities/validatorUtilities.js
@@ -0,0 +1,25 @@
+import _isArray from 'lodash/isArray';
+import _isEmpty from 'lodash/isEmpty';
+import _isObject from 'lodash/isObject';
+
+/**
+ * This will return true if it is not an object or it is empty
+ *
+ * @funtion isNotObjectOrIsEmpty
+ * @param input {object}
+ * @return {boolean}
+ */
+export const isNotObjectOrIsEmpty = (value) => {
+    return !_isObject(value) || _isEmpty(value);
+};
+
+/**
+ * This will return true if it is not an object or it is empty or is an array
+ *
+ * @funtion isNotObjectOrIsEmptyOrIsArray
+ * @param input {object}
+ * @return {boolean}
+ */
+export const isNotObjectOrIsEmptyOrIsArray = (value) => {
+    return !_isObject(value) || _isEmpty(value)  || _isArray(value);
+};
