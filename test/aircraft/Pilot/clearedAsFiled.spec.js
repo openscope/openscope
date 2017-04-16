95d1075053e5e5d3ef3545a81f55acb2073aafd3
Erik Quinn <erikquinn2@gmail.com>
2017-04-16 00:47:19 -0400

    feature/139 - Update tests for Pilot.clearedAsFiled()

--- test/aircraft/Pilot/clearedAsFiled.spec.js
+++ test/aircraft/Pilot/clearedAsFiled.spec.js
@@ -8,48 +8,16 @@ import {
     fmsArrivalFixture,
     modeControllerFixture
 } from '../../fixtures/aircraftFixtures';
-import { airportModelFixture } from '../../fixtures/airportFixtures';
 
-const runwayMock = '19L';
-const cruiseSpeedMock = 460;
 
-ava('.clearedAsFiled() should set mcp altitude, heading and speed modes with values', (t) => {
-    const expectedResult = {
-        altitudeMode: 'HOLD',
-        autopilotMode: 'OFF',
-        headingMode: 'LNAV',
-        speedMode: 'N1',
-        altitude: 19000,
-        course: -1,
-        heading: 3.367484597776187,
-        speed: 460,
-        shouldExpediteAltitudeChange: false
-    };
-    const initialAltitudeMock = airportModelFixture.initial_alt;
-    const { angle: runwayHeadingMock } = airportModelFixture.getRunway(runwayMock);
+ava('.clearedAsFiled() grants pilot departure clearance and returns the correct response strings', (t) => {
     const pilot = new Pilot(modeControllerFixture, fmsArrivalFixture);
-
-    pilot.clearedAsFiled(initialAltitudeMock, runwayHeadingMock, cruiseSpeedMock);
-
-    t.true(pilot._mcp.isEnabled);
-    t.true(pilot._mcp.altitudeMode === expectedResult.altitudeMode);
-    t.true(pilot._mcp.headingMode === expectedResult.headingMode);
-    t.true(pilot._mcp.speedMode === expectedResult.speedMode);
-    t.true(pilot._mcp.altitude === expectedResult.altitude);
-    t.true(pilot._mcp.course === expectedResult.course);
-    t.true(pilot._mcp.heading === expectedResult.heading);
-    t.true(pilot._mcp.speed === expectedResult.speed);
-});
-
-ava('.clearedAsFiled() should returns the correct response strings', (t) => {
-    const initialAltitudeMock = airportModelFixture.initial_alt;
-    const { angle: runwayHeadingMock } = airportModelFixture.getRunway(runwayMock);
-    const pilot = new Pilot(modeControllerFixture, fmsArrivalFixture);
-    const result = pilot.clearedAsFiled(initialAltitudeMock, runwayHeadingMock, cruiseSpeedMock);
+    const result = pilot.clearedAsFiled();
 
     t.true(_isArray(result));
     t.true(result[0] === true);
     t.true(_isObject(result[1]));
-    t.true(result[1].log === 'cleared to destination as filed. Climb and maintain 19000, expect 41000 10 minutes after departure');
-    t.true(result[1].say === 'cleared to destination as filed. Climb and maintain flight level one niner zero, expect flight level four one zero, one zero minutes after departure');
+    t.true(result[1].log === 'cleared to destination as filed');
+    t.true(result[1].say === 'cleared to destination as filed');
+    t.true(pilot.hasDepartureClearance === true);
 });
