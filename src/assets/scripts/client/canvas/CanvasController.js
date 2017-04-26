25f37827e01ee87835fcbc84cdab72f17d0b3856
Erik Quinn <erikquinn2@gmail.com>
2017-04-26 12:36:10 -0400

    feature/317 - Move canvas constants to own file

--- src/assets/scripts/client/canvas/CanvasController.js
+++ src/assets/scripts/client/canvas/CanvasController.js
@@ -11,59 +11,14 @@ import { vscale, vturn, positive_intersection_with_rect } from '../math/vector';
 import { SELECTORS } from '../constants/selectors';
 import { LOG } from '../constants/logLevel';
 import { FLIGHT_PHASE, FLIGHT_CATEGORY } from '../constants/aircraftConstants';
+import {
+    BASE_CANVAS_FONT,
+    COLORS,
+    DEFAULT_CANVAS_SIZE
+} from '../constants/canvasConstants';
 
 // Temporary const declaration here to attach to the window AND use as internal property
 const canvas = {};
-
-/**
- * @property BASE_CANVAS_FONT
- * @type {string}
- * @final
- */
-const BASE_CANVAS_FONT = '10px monoOne, monospace';
-
-/**
- * @property DEFAULT_CANVAS_SIZE
- * @type {Object}
- * @final
- */
-const DEFAULT_CANVAS_SIZE = {
-    HEIGHT: 480,
-    WIDTH: 640
-};
-
-/**
- * @property COLORS
- * @type {Object}
- * @final
- */
-const COLORS = {
-    WHITE_00: 'rgba(255, 255, 255, 0)',
-    WHITE_02: 'rgba(255, 255, 255, 0.2)',
-    WHITE_03: 'rgba(255, 255, 255, 0.2)',
-    WHITE_04: 'rgba(255, 255, 255, 0.4)',
-    WHITE_05: 'rgba(255, 255, 255, 0.5)',
-    WHITE_07: 'rgba(255, 255, 255, 0.7)',
-    WHITE_08: 'rgba(255, 255, 255, 0.8)',
-    WHITE: 'rgba(255, 255, 255, 1.0)',
-    LIGHT_SILVER_03: 'rgba(224, 224, 224, 0.3)',
-    LIGHT_SILVER: 'rgba(224, 224, 224, 1.0)',
-    RED_06: 'rgba(224, 128, 128, 0.6)',
-    RED_08: 'rgba(224, 128, 128, 0.8)',
-    RED: 'rgba(224, 128, 128, 1.0)',
-    CORAL_RED: 'rgba(255, 64, 64, 1.0)',
-    BRIGHT_RED: 'rgba(255, 0, 0, 0.7)',
-    GRAIN_BROWN: 'rgba(224, 210, 180, 1.0)',
-    DEPARTURE_COLOR: 'rgba(128, 255, 255, 0.6)',
-    VIDEO_MAP_STROKE: 'rgba(193, 218, 205, 1)',
-    DEPARTURE_STROKE: 'rgba(128, 255, 255, 0.9)',
-    AIRSPACE_RADIUS_FILL: 'rgba(200, 255, 200, 0.02)',
-    AIRSPACE_RADIUS_STROKE: 'rgba(200, 255, 200, 0.25)',
-    AIRSPACE_RANGE_RING_COLOR: 'rgba(200, 255, 200, 0.1)',
-    CROSSHAIR_STROKE: 'rgba(136, 153, 153, 1)',
-    RESTRICTED_AIRSPACE_STROKE: 'rgba(150, 200, 255, 0.3)',
-    RESTRICTED_AIRSPACE_FILL: 'rgba(150, 200, 255, 0.4)'
-};
 
 /**
  * @class CanvasController
---
+++ src/assets/scripts/client/constants/canvasConstants.js
@@ -0,0 +1,50 @@
+/**
+ * @enum BASE_CANVAS_FONT
+ * @type {string}
+ * @final
+ */
+export const BASE_CANVAS_FONT = '10px monoOne, monospace';
+
+/**
+ * @property COLORS
+ * @type {Object}
+ * @final
+ */
+export const COLORS = {
+    WHITE_00: 'rgba(255, 255, 255, 0)',
+    WHITE_02: 'rgba(255, 255, 255, 0.2)',
+    WHITE_03: 'rgba(255, 255, 255, 0.2)',
+    WHITE_04: 'rgba(255, 255, 255, 0.4)',
+    WHITE_05: 'rgba(255, 255, 255, 0.5)',
+    WHITE_07: 'rgba(255, 255, 255, 0.7)',
+    WHITE_08: 'rgba(255, 255, 255, 0.8)',
+    WHITE: 'rgba(255, 255, 255, 1.0)',
+    LIGHT_SILVER_03: 'rgba(224, 224, 224, 0.3)',
+    LIGHT_SILVER: 'rgba(224, 224, 224, 1.0)',
+    RED_06: 'rgba(224, 128, 128, 0.6)',
+    RED_08: 'rgba(224, 128, 128, 0.8)',
+    RED: 'rgba(224, 128, 128, 1.0)',
+    CORAL_RED: 'rgba(255, 64, 64, 1.0)',
+    BRIGHT_RED: 'rgba(255, 0, 0, 0.7)',
+    GRAIN_BROWN: 'rgba(224, 210, 180, 1.0)',
+    DEPARTURE_COLOR: 'rgba(128, 255, 255, 0.6)',
+    VIDEO_MAP_STROKE: 'rgba(193, 218, 205, 1)',
+    DEPARTURE_STROKE: 'rgba(128, 255, 255, 0.9)',
+    AIRSPACE_RADIUS_FILL: 'rgba(200, 255, 200, 0.02)',
+    AIRSPACE_RADIUS_STROKE: 'rgba(200, 255, 200, 0.25)',
+    AIRSPACE_RANGE_RING_COLOR: 'rgba(200, 255, 200, 0.1)',
+    CROSSHAIR_STROKE: 'rgba(136, 153, 153, 1)',
+    RESTRICTED_AIRSPACE_STROKE: 'rgba(150, 200, 255, 0.3)',
+    RESTRICTED_AIRSPACE_FILL: 'rgba(150, 200, 255, 0.4)'
+};
+
+
+/**
+ * @enum DEFAULT_CANVAS_SIZE
+ * @type {Object}
+ * @final
+ */
+export const DEFAULT_CANVAS_SIZE = {
+    HEIGHT: 480,
+    WIDTH: 640
+};
