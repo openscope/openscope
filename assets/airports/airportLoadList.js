bfeca19570a80802d07f28b11ffd030f2fc7a63e
Ilpo Järvinen <ilpo.jarvinen@helsinki.fi>
2017-05-26 23:18:25 +0300

    feature/494 - Enable ESPA again

--- assets/airports/airportLoadList.js
+++ assets/airports/airportLoadList.js
@@ -137,12 +137,12 @@ window.AIRPORT_LOAD_LIST = (function() { // eslint-disable-line wrap-iife
             name: 'Oslo Gardermoen International Airport',
             wip: true
         },
-        // {
-        //     icao: 'espa',
-        //     level: 'easy',
-        //     name: 'Luleå Airport',
-        //     wip: true
-        // },
+        {
+            icao: 'espa',
+            level: 'easy',
+            name: 'Luleå Airport',
+            wip: true
+        },
         {
             icao: 'gcrr',
             level: 'easy',
