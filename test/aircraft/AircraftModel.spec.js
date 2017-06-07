feb714f2621192854d0dcf4f42826cfc368c288b
Nate Geslin <teamtomkins23@gmail.com>
2017-06-06 21:41:05 -0500

    feature/40 - adds falsy test case to AircraftModel.matchCallsign()

--- test/aircraft/AircraftModel.spec.js
+++ test/aircraft/AircraftModel.spec.js
@@ -11,10 +11,17 @@ ava('does not throw with valid parameters', (t) => {
     t.notThrows(() => new AircraftModel(DEPARTURE_AIRCRAFT_INIT_PROPS_MOCK, navigationLibraryFixture));
 });
 
+
 ava('.matchCallsign() returns true when passed `*`', (t) => {
     const model = new AircraftModel(DEPARTURE_AIRCRAFT_INIT_PROPS_MOCK, navigationLibraryFixture);
 
     t.true(model.matchCallsign('*'));
+});
+
+ava('.matchCallsign() returns false when passed a flightnumber that is not included in #callsign', (t) => {
+    const model = new AircraftModel(DEPARTURE_AIRCRAFT_INIT_PROPS_MOCK, navigationLibraryFixture);
+
+    t.false(model.matchCallsign('42'));
 });
 
 ava('.matchCallsign() returns true when passed a flightnumber that is included in #callsign', (t) => {
