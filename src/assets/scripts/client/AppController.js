f7b78a4f042adfd4bb343c02d77402449cfa648e
Erik Quinn <erikquinn2@gmail.com>
2017-08-18 12:58:04 -0400

    refactor/431 - Make 'draw fixes' toggle button always visible

--- src/assets/scripts/client/AppController.js
+++ src/assets/scripts/client/AppController.js
@@ -281,7 +281,6 @@ export default class AppController {
 
         this.canvasController.canvas.dirty = true;
 
-        $(SELECTORS.DOM_SELECTORS.TOGGLE_LABELS).toggle(!_isEmpty(airport.maps));
         $(SELECTORS.DOM_SELECTORS.TOGGLE_RESTRICTED_AREAS).toggle((airport.restricted_areas || []).length > 0);
         $(SELECTORS.DOM_SELECTORS.TOGGLE_SIDS).toggle(!_isNil(this.navigationLibrary.sidCollection));
         $(SELECTORS.DOM_SELECTORS.TOGGLE_TERRAIN).toggle(airport.data.has_terrain);
