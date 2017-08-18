9dbdcb87a5d4e5fa5e8eaa19d00e26cbf042dc09
Erik Quinn <erikquinn2@gmail.com>
2017-08-18 13:09:52 -0400

    refactor/431 - Deactivate EHAM due to looping errors

--- assets/airports/airportLoadList.js
+++ assets/airports/airportLoadList.js
@@ -118,13 +118,13 @@ window.AIRPORT_LOAD_LIST = (function() { // eslint-disable-line wrap-iife
             premium: false,
             wip: true
         },
-        {
-            icao: 'eham',
-            level: 'medium',
-            name: 'Amsterdam Airport Schiphol',
-            premium: false,
-            wip: true
-        },
+        // {
+        //     icao: 'eham',
+        //     level: 'medium',
+        //     name: 'Amsterdam Airport Schiphol',
+        //     premium: false,
+        //     wip: true
+        // },
         {
             icao: 'eidw',
             level: 'easy',
