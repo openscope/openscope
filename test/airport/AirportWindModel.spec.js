c455a92bda0b8516cecc7dc5ccbbe079373620aa
sam-irl <therealsamave@gmail.com>
2017-10-04 13:36:05 -0500

    enhancement/4 - everything is stupid and I hate this

--- test/airport/AirportWindModel.spec.js
+++ test/airport/AirportWindModel.spec.js
@@ -23,20 +23,3 @@ ava('._calculateNextWind does not give negative values for speed or angle', (t) 
     t.true(result.speed >= 0);
     t.true(result.angle >= 0);
 });
-
-ava('AircraftWindModel will set to default values if not given `speed` and `angle`', (t) => {
-    const noSpeedWind = {
-        angle: 200
-    };
-    const noAngleWind = {
-        speed: 15
-    };
-    const noParamsWindModel = new AirportWindModel();
-    const noSpeedWindModel = new AirportWindModel(noSpeedWind);
-    const noAngleWindModel = new AirportWindModel(noAngleWind);
-
-    t.true(noParamsWindModel.speed === 10);
-    t.true(noParamsWindModel.angle === 0);
-    t.true(noSpeedWindModel.speed === 10);
-    t.true(noAngleWindModel.angle === 0);
-});
