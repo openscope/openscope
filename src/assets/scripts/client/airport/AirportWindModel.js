a83c3f0c6c0ab834163d3bb2a45374b8f73a99b3
Nate Geslin <teamtomkins23@gmail.com>
2017-10-04 18:42:10 -0500

    enhancement/4 - updates method binding for interval callback * setups up additional lifecycle methods to setup AirportWindModel instance

--- src/assets/scripts/client/airport/AirportWindModel.js
+++ src/assets/scripts/client/airport/AirportWindModel.js
@@ -3,30 +3,49 @@ import _round from 'lodash/round';
 import GameController from '../game/GameController';
 import { calculateNormalDistributedNumber } from '../math/core';
 
+/**
+ *
+ *
+ * @class AirportWindModel
+ */
 export default class AirportWindModel {
     /**
-     * Constructor. Sets up the class with
-     * invalid properties.
-     * 
      * @constructor
+     * @param initialAirportWind {object}
      */
-    constructor(data) {
+    constructor(initialAirportWind) {
+        /**
+         *
+         *
+         * @proeprty angle
+         * @type {number}
+         */
         this.angle = 0;
+
+        /**
+         *
+         *
+         * @proeprty speed
+         * @type {number}
+         */
         this.speed = 10;
 
         /**
          * Instance of the running schedule.
-         * 
+         *
          * @type {gameTimeout|null}
          */
         this.currentSchedule = null;
 
-        return this._init(data);
+        return this._init(initialAirportWind)
+            ._setupHandlers()
+            .enable();
     }
 
     /**
      * Method to initialize default wind values.
-     * 
+     *
+     * @for AirportWindModel
      * @method _init
      * @param {Object} data - airport-specific defaults from AirportModel
      * @private
@@ -35,12 +54,40 @@ export default class AirportWindModel {
         this.speed = data.speed;
         this.angle = data.angle;
 
-        return this._createWindUpdateTimer();
+        return this;
+    }
+
+    /**
+     * Setup handler methods with proper scope binding
+     *
+     * @for AirportWindModel
+     * @method _setupHandlers
+     * @chainable
+     * @private
+     */
+    _setupHandlers() {
+        this._onCalculateNextWindHandler = this._calculateNextWind.bind(this);
+
+        return this;
+    }
+
+    /**
+     * Enable the instance
+     *
+     * @for AirportWindModel
+     * @method enable
+     * @chainable
+     */
+    enable() {
+        this._createWindUpdateTimer();
+
+        return this;
     }
 
     /**
      * Actual math function to find the wind on a bell curve.
-     * 
+     *
+     * @for AirportWindModel
      * @method calculateNextWind
      * @private
      */
@@ -49,28 +96,29 @@ export default class AirportWindModel {
         const speed = _round(calculateNormalDistributedNumber(this.speed));
         const initialAngle = _round(calculateNormalDistributedNumber(this.angle));
         let nextAngle = initialAngle;
-    
+
         if (initialAngle > 360) {
             // How many times can we subtract 360 and not get a negative number?
             const factorsOfThreeSixty = _floor(initialAngle / 360);
-    
+
             nextAngle = initialAngle - (360 * factorsOfThreeSixty);
         }
-    
+
         this.speed = speed;
         this.angle = nextAngle;
     }
 
     /**
-     * Creates the timer to update the wind.
-     * 
+     * Creates an interval timer that will call `._calculateNextWind()`
+     *
+     * @for AirportWindModel
      * @method createWindUpdateTimer
-     * @private
      * @return {gameTimeout} an instance of the new game timeout.
+     * @private
      */
     _createWindUpdateTimer() {
-        return GameController.game_interval(
-            this._calculateNextWind(),
+        GameController.game_interval(
+            this._onCalculateNextWindHandler,
             300,
             null,
             null
