79fcfcd782f9d2c8356a3b53b6bcf3014d4b58c4
Fechulo <fhandl@gmail.com>
2017-10-06 15:58:25 +0100

    refactor/427 - wip: false

--- assets/airports/airportLoadList.js
+++ assets/airports/airportLoadList.js
@@ -330,7 +330,7 @@ window.AIRPORT_LOAD_LIST = (function() { // eslint-disable-line wrap-iife
             level: 'medium',
             name: 'San Francisco International Airport',
             premium: false,
-            wip: true
+            wip: false
         },
         {
             icao: 'kstl',
