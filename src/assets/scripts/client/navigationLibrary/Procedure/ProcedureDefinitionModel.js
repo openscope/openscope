eb9485499509ceb2f0e0206635a4121ccb683372
Erik Quinn <erikquinn2@gmail.com>
2017-11-12 11:40:51 -0500

    refactor/809 - Complete all docblocks for ProcedureDefinitionModel

--- src/assets/scripts/client/navigationLibrary/Procedure/ProcedureDefinitionModel.js
+++ src/assets/scripts/client/navigationLibrary/Procedure/ProcedureDefinitionModel.js
@@ -1,5 +1,3 @@
-// import _has from 'lodash/has';
-// import _flatten from 'lodash/flatten';
 import _forEach from 'lodash/forEach';
 import _isArray from 'lodash/isArray';
 import _map from 'lodash/map';
@@ -11,7 +9,7 @@ import { PROCEDURE_TYPE } from '../../constants/aircraftConstants';
 /**
  * Generic class for instrument procedures of multiple types, such as SIDs/STARs
  *
- * Used by FMS to generate waypoints from procedure definitions outlined in an
+ * Used by FMS to generate waypoints from instrument procedures outlined in an
  * airport's JSON file. The FMS is given a route on spawn, or a reroute by a
  * controller, and if it contains a procedure, it will look up the associated
  * `ProcedureDefinitionModel`, and request the waypoints for that procedure at
@@ -36,76 +34,238 @@ export default class ProcedureDefinitionModel {
             throw new TypeError(`Expected procedure of known type, but received procedure type of '${procedureType}'`);
         }
 
-        this.procedureType = procedureType;
+        /**
+         * Body segment of the procedure
+         *
+         * All possible routes will contain all waypoints from the body, regardless
+         * of where they enter or exit the procedure. Does not necessarily contain
+         * any waypoints, provided that no combination of entry/exit would result
+         * in fewer than two waypoints.
+         *
+         * @for ProcedureDefinitionModel
+         * @property _body
+         * @type {array<array<string>|<string>>}
+         * @default []
+         * @private
+         */
+        this._body = [];
+
+        /**
+         * 2D array describing the lines needed to be drawn between fixes in order
+         * to properly depict the procedure's path on the scope
+         *
+         * [
+         *     ['FIXXA', 'FIXXB', 'FIXXC'],
+         *     ['FIXXA', 'FIXXC']
+         * ]
+         *
+         * @for ProcedureDefinitionModel
+         * @property _draw
+         * @type {array<array<string>>}
+         * @default []
+         * @private
+         */
+        this._draw = [];
+
+        /**
+         * All fixes where aircraft may enter the procedure
+         *
+         * Each entry fix is a key in this property, whose value is a list of (restrictable)
+         * fixes to follow on that entry in order to join the body of the procedure.
+         *
+         * @for ProcedureDefinitionModel
+         * @property _entryPoints
+         * @type {array<array<string>|<string>>}
+         * @default {}
+         * @private
+         */
+        this._entryPoints = {};
+
+        /**
+         * All fixes where aircraft may exit the procedure
+         *
+         * Each exit fix is a key in this property, whose value is a list of (restrictable)
+         * fixes to follow on that exit in order to leave the procedure.
+         *
+         * @for ProcedureDefinitionModel
+         * @property _exitPoints
+         * @type {array<array<string>|<string>>}
+         * @default {}
+         * @private
+         */
+        this._exitPoints = {};
+
+        /**
+         * The ICAO identifier for this procedure
+         *
+         * @for ProcedureDefinitionModel
+         * @property _icao
+         * @type {string}
+         * @default ''
+         * @private
+         */
+        this._icao = '';
+
+        /**
+         * The verbally spoken name of the procedure
+         *
+         * Nonstandard spellings may be used to achieve the desired pronunciations,
+         * since this is only used for speech synthesis.
+         *
+         * @for ProcedureDefinitionModel
+         * @property _name
+         * @type {string}
+         * @default ''
+         * @private
+         */
+        this._name = '';
+
+        /**
+         * The type of instrument procedure (must be one of `PROCEDURE_TYPE`)
+         *
+         * @for ProcedureDefinitionModel
+         * @property _procedureType
+         * @type {string}
+         * @default ''
+         * @private
+         */
+        this._procedureType = '';
+
+        this.init(procedureType, data);
+    }
+
+    /**
+     * Return value of `#_draw`
+     *
+     * @for ProcedureDefinitionModel
+     * @property draw
+     * @type {array}
+     */
+    get draw() {
+        return this._draw;
+    }
+
+    /**
+     * Return value of `#_icao`
+     *
+     * @for ProcedureDefinitionModel
+     * @property icao
+     * @type {string}
+     */
+    get icao() {
+        return this._icao;
+    }
+
+    /**
+     * Return value of `#_name`
+     *
+     * @for ProcedureDefinitionModel
+     * @property name
+     * @type {string}
+     */
+    get name() {
+        return this._name;
+    }
+
+    /**
+     * Return value of `#_procedureType`
+     *
+     * @for ProcedureDefinitionModel
+     * @property procedureType
+     * @type {string}
+     */
+    get procedureType() {
+        return this._procedureType;
+    }
+
+    // ------------------------------ LIFECYCLE ------------------------------
+
+    /**
+     * Initialize class properties
+     *
+     * @for ProcedureDefinitionModel
+     * @method init
+     * @param procedureType {string} must belong to the `PROCEDURE_TYPE` enum
+     * @param data {object} JSON data from airport file
+     * @chainable
+     */
+    init(procedureType, data) {
+        this._body = data.body;
+        this._draw = data.draw;
+        this._icao = data.icao;
+        this._name = data.name;
+        this._procedureType = procedureType;
+
+        if (this._procedureType === PROCEDURE_TYPE.SID) {
+            return this._initEntriesAndExitsForSid(data);
+        } else if (this._procedureType === PROCEDURE_TYPE.STAR) {
+            return this._initEntriesAndExitsForStar(data);
+        }
+
+        throw new TypeError('Expected procedure definition with known type, ' +
+            `but received type '${this._procedureType}'`);
+    }
+
+    /**
+     * Reset class properties
+     *
+     * @for ProcedureDefinitionModel
+     * @method reset
+     * @chainable
+     */
+    reset() {
         this._body = [];
         this._draw = [];
         this._entryPoints = {};
         this._exitPoints = {};
         this._icao = '';
         this._name = '';
-
-        this._init(data);
-    }
-
-    /**
-     * Populate properties from JSON data
-     *
-     * @for ProcedureDefinitionModel
-     * @method _init
-     * @private
-     * @chainable
-     */
-    _init(data) {
-        this._body = data.body;
-        this._draw = data.draw;
-        this._icao = data.icao;
-        this._name = data.name;
-
-        if (this.procedureType === PROCEDURE_TYPE.SID) {
-            this._initEntriesAndExitsForSid(data);
-
-            return this;
-        }
-
-        this._initEntriesAndExitsForStar(data);
+        this._procedureType = '';
 
         return this;
     }
 
     /**
-     * Initialize `#entries` and `#exits` for 'SID' procedure
+     * Initialize `#_entryPoints` and `#_exitPoints` for 'SID' procedure
      *
      * @for ProcedureDefinitionModel
      * @method _initEntriesAndExitsForSid
      * @param data {object} JSON data from airport file
      * @private
+     * @chainable
      */
     _initEntriesAndExitsForSid(data) {
         this._entryPoints = data.rwy;
         this._exitPoints = data.exitPoints;
+
+        return this;
     }
 
     /**
-     * Initialize `#entries` and `#exits` for 'STAR' procedure
+     * Initialize `#_entryPoints` and `#_exitPoints` for 'STAR' procedure
      *
      * @for ProcedureDefinitionModel
      * @method _initEntriesAndExitsForStar
      * @param data {object} JSON data from airport file
      * @private
+     * @chainable
      */
     _initEntriesAndExitsForStar(data) {
         this._entryPoints = data.entryPoints;
         this._exitPoints = data.rwy;
+
+        return this;
     }
 
-    hasEntry(entryName) {
-        return entryName in this._entryPoints;
-    }
+    // ------------------------------ PUBLIC ------------------------------
 
-    hasExit(exitName) {
-        return exitName in this._exitPoints;
-    }
-
+    /**
+     * Return an array of names of all fixes existing in any segment of this procedure
+     *
+     * @for ProcedureDefinitionModel
+     * @method getAllFixNamesInUse
+     * @return {array<string>}
+     */
     getAllFixNamesInUse() {
         if (!_isArray(this._draw[0])) {
             throw new TypeError(`Invalid data set in draw segment of the ${this._icao} procedure. Expected a 2D ` +
@@ -124,6 +284,13 @@ export default class ProcedureDefinitionModel {
         return uniqueFixNames;
     }
 
+    /**
+     * Return the name of a randomly selected exit point
+     *
+     * @for ProcedureDefinitionModel
+     * @method getRandomExitPoint
+     * @return {string}
+     */
     getRandomExitPoint() {
         const exitNames = Object.keys(this._exitPoints);
         const maxIndex = exitNames.length - 1;
@@ -162,7 +329,31 @@ export default class ProcedureDefinitionModel {
     }
 
     /**
-    * Generate new `WaypointModel`s for the body portion of the procedure
+     * Return whether this procedure contains an entry point with the specified name
+     *
+     * @for ProcedureDefinitionModel
+     * @method hasEntry
+     * @return {boolean}
+     */
+    hasEntry(entryName) {
+        return entryName in this._entryPoints;
+    }
+
+    /**
+     * Return whether this procedure contains an exit point with the specified name
+     *
+     * @for ProcedureDefinitionModel
+     * @method hasExit
+     * @return {boolean}
+     */
+    hasExit(exitName) {
+        return exitName in this._exitPoints;
+    }
+
+    // ------------------------------ PRIVATE ------------------------------
+
+    /**
+    * Generate new `ProcedureWaypointModel`s for the body portion of the procedure
     *
     * @for ProcedureDefinitionModel
     * @method _generateWaypointsForBody
@@ -174,7 +365,7 @@ export default class ProcedureDefinitionModel {
     }
 
     /**
-    * Generate new `WaypointModel`s for the specified entry
+    * Generate new `ProcedureWaypointModel`s for the specified entry
     *
     * @for ProcedureDefinitionModel
     * @method _generateWaypointsForEntry
@@ -191,7 +382,7 @@ export default class ProcedureDefinitionModel {
     }
 
     /**
-    * Generate new `WaypointModel`s for the specified exit
+    * Generate new `ProcedureWaypointModel`s for the specified exit
     *
     * @for ProcedureDefinitionModel
     * @method _generateWaypointsForEntry
@@ -207,12 +398,28 @@ export default class ProcedureDefinitionModel {
         return _map(this._exitPoints[exitPoint], (waypoint) => new ProcedureWaypointModel(waypoint));
     }
 
+    /**
+     * Return an array containing names of all fixes existing in the body
+     *
+     * @for ProcedureDefinitionModel
+     * @method _getFixNamesFromBody
+     * @return {array<string>}
+     * @private
+     */
     _getFixNamesFromBody() {
         return _map(this._body, (restrictedFix) => {
             return this._getFixNameFromRestrictedFixArray(restrictedFix);
         });
     }
 
+    /**
+     * Return an array containing names of all fixes existing in the draw array
+     *
+     * @for ProcedureDefinitionModel
+     * @method _getFixNamesFromDraw
+     * @return {array<string>}
+     * @private
+     */
     _getFixNamesFromDraw() {
         const drawFixNames = this._draw.reduce((fixList, lineSegment) => fixList.concat(lineSegment));
         const drawFixNamesWithoutAsterisks = drawFixNames.map((fixName) => fixName.replace('*', ''));
@@ -220,6 +427,14 @@ export default class ProcedureDefinitionModel {
         return drawFixNamesWithoutAsterisks;
     }
 
+    /**
+     * Return an array containing names of all fixes existing in any entry
+     *
+     * @for ProcedureDefinitionModel
+     * @method _getFixNamesFromEntries
+     * @return {array<string>}
+     * @private
+     */
     _getFixNamesFromEntries() {
         let fixNames = [];
 
@@ -234,6 +449,14 @@ export default class ProcedureDefinitionModel {
         return _uniq(fixNames);
     }
 
+    /**
+     * Return an array containing names of all fixes existing in any exit
+     *
+     * @for ProcedureDefinitionModel
+     * @method _getFixNamesFromExits
+     * @return {array<string>}
+     * @private
+     */
     _getFixNamesFromExits() {
         let fixNames = [];
 
@@ -248,6 +471,17 @@ export default class ProcedureDefinitionModel {
         return _uniq(fixNames);
     }
 
+    /**
+     * Return the name of a fix from a restrited-fix array
+     *
+     * Ex:    ['FIXXA', 'A100']    -->    'FIXXA'
+     *
+     * @for ProcedureDefinitionModel
+     * @method _getFixNameFromRestrictedFixArray
+     * @param restrictedFix {array<string>}
+     * @return {string}
+     * @private
+     */
     _getFixNameFromRestrictedFixArray(restrictedFix) {
         if (_isArray(restrictedFix)) {
             restrictedFix = restrictedFix[0];
