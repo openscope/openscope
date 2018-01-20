af70726b3dd5ed1bdc8eaa05989882c6d4847e4e
indianbhaji <indianbhaji@gmail.com>
2018-01-20 14:27:15 +0000

    enhancement/866 - rename functions, simplify logic

--- src/assets/scripts/client/airline/flightNumberBuilder.js
+++ src/assets/scripts/client/airline/flightNumberBuilder.js
@@ -7,24 +7,18 @@ const NUMERIC = '0123456789';
  * This picks a random number. If it is the first value within the callsign (ie. i === 0), then it picks a
  * number between 1 and 9. Otherwise, it picks a number between 0 and 9.
 */
-const getNumericValue = (i) => {
-    let randomNumber = 1;
-
-    switch (i) {
-        case 0:
-            randomNumber = choose(NUMERIC.substr(1));
-            break;
-        default:
-            randomNumber = choose(NUMERIC);
+const generateNumericValue = (i) => {
+    if (i === 0) {
+        return choose(NUMERIC.substr(1));
     }
 
-    return randomNumber;
+    return choose(NUMERIC);
 };
 
 /**
  * Self-explanatory; this function picks a random letter and returns it.
 */
-const getAlphaValue = () => {
+const generateAlphaValue = () => {
     return choose(ALPHA);
 };
 
@@ -32,7 +26,7 @@ const getAlphaValue = () => {
  * Accepts a list of callsign formats, which are defined in the airline files. It randomly selects one of these
  * formats and generates a callsign based off this format.
  *
- * @function generateFlightNumber
+ * @function flightNumberBuilder
  * @param callsignFormats {array}
  * @return {string}
  */
@@ -43,10 +37,10 @@ export const flightNumberBuilder = (callsignFormats) => {
     for (let i = 0; i < chosenFormat.length; i++) {
         switch (chosenFormat[i]) {
             case '#':
-                flightNumber += getNumericValue(i);
+                flightNumber += generateNumericValue(i);
                 break;
             case 'A':
-                flightNumber += getAlphaValue();
+                flightNumber += generateAlphaValue();
                 break;
             default:
                 console.warn(`${this.icao} has an incorrect callsign format, it should only contain 'A' or '#'`);
