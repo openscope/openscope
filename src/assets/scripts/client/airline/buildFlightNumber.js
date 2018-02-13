2584059ed4eba3d49e1c6de27f9ed4cd52a6a6e1
indianbhaji <indianbhaji@gmail.com>
2018-02-13 12:43:39 +0000

    enhancement/866 - use `continue` and create constants

--- src/assets/scripts/client/airline/buildFlightNumber.js
+++ src/assets/scripts/client/airline/buildFlightNumber.js
@@ -1,5 +1,9 @@
 import { choose } from '../utilities/generalUtilities';
-import { DEFAULT_CALLSIGN_FORMAT } from '../constants/airlineConstants';
+import {
+    DEFAULT_CALLSIGN_FORMAT,
+    CALLSIGN_RANDOM_DIGIT_CHARACTER,
+    CALLSIGN_RANDOM_LETTER_CHARACTER
+} from '../constants/airlineConstants';
 
 const ALPHA = 'abcdefghijklmnopqrstuvwxyz';
 const NUMERIC = '0123456789';
@@ -8,6 +12,7 @@ const defaultCallsignFormats = [DEFAULT_CALLSIGN_FORMAT];
 /**
  * This picks a random number. If it is the first value within the callsign (ie. i === 0), then it picks a
  * number between 1 and 9. Otherwise, it picks a number between 0 and 9.
+ *
  * @function _generateRandomDigit
  * @param i {number}
  * @return NUMERIC {string}
@@ -22,6 +27,7 @@ function _generateRandomDigit(i) {
 
 /**
  * This function picks a random letter and returns it.
+ *
  * @function _generateRandomLetter
  * @return ALPHA {string}
 */
@@ -32,9 +38,10 @@ function _generateRandomLetter() {
 /**
  * This function takes a list of callsignFormats, validates them (ie. ensures that no callsignFormat has a leading 0)
  * then returns a list of validated callsignFormats or the default callsignFormat if there are no valid callsignFormats
+ *
  * @function _validateCallsignFormats
- * @param callsignFormat {Array}
- * @return validatedFormats {Array}
+ * @param callsignFormat {array<string>}
+ * @return validatedFormats {array<string>}
 */
 function _validateCallsignFormats(callsignFormats) {
     const validatedFormats = [];
@@ -42,9 +49,10 @@ function _validateCallsignFormats(callsignFormats) {
     for (let i = 0; i < callsignFormats.length; i++) {
         if (callsignFormats[i].charAt(0) === '0') {
             console.warn(`Format ${callsignFormats[i]} is invalid as it has a leading zero!`);
-        } else {
-            validatedFormats.push(callsignFormats[i]);
+            continue;
         }
+
+        validatedFormats.push(callsignFormats[i]);
     }
 
     if (validatedFormats.length === 0) {
@@ -59,7 +67,7 @@ function _validateCallsignFormats(callsignFormats) {
  * formats and generates a callsign based off this format.
  *
  * @function buildFlightNumber
- * @param callsignFormats {array}
+ * @param callsignFormats {array<string>}
  * @return {string}
 */
 export function buildFlightNumber(callsignFormats) {
@@ -70,10 +78,10 @@ export function buildFlightNumber(callsignFormats) {
 
     for (let i = 0; i < chosenFormat.length; i++) {
         switch (chosenFormat[i]) {
-            case '#':
+            case CALLSIGN_RANDOM_DIGIT_CHARACTER:
                 flightNumber += _generateRandomDigit(i);
                 break;
-            case '@':
+            case CALLSIGN_RANDOM_LETTER_CHARACTER:
                 flightNumber += _generateRandomLetter();
                 break;
             default:
