6ab478814d5979250a5fb73071a805c5a2959a68
jakcharvat <jakcharvat@gmail.com>
2018-02-16 22:18:02 +0800

    feature/lkpr -

--- assets/airports/airportLoadList.js
+++ assets/airports/airportLoadList.js
@@ -400,12 +400,12 @@ window.AIRPORT_LOAD_LIST = (function() { // eslint-disable-line wrap-iife
         //     premium: false,
         //     wip: false
         // },
-        // {
-        //     icao: 'lkpr',
-        //     level: 'easy',
-        //     name: 'Vaclav Havel International Airport',
-        //     premium: false,
-        //     wip: true
+        {
+            icao: 'lkpr',
+            level: 'easy',
+            name: 'Vaclav Havel International Airport',
+            premium: false,
+            wip: true
         // },
         // {
         //     icao: 'loww',
