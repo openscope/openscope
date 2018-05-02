374b82ce0ed8efedbc827abd91b8eb0e28276a5c
Erik Quinn <erikquinn2@gmail.com>
2018-05-02 03:03:06 -0500

    feature/893 - Set wip to false for LKPR to enable loading

--- assets/airports/airportLoadList.js
+++ assets/airports/airportLoadList.js
@@ -405,7 +405,7 @@ window.AIRPORT_LOAD_LIST = (function() { // eslint-disable-line wrap-iife
             level: 'easy',
             name: 'Vaclav Havel International Airport',
             premium: false,
-            wip: true
+            wip: false
         },
         // {
         //     icao: 'loww',
