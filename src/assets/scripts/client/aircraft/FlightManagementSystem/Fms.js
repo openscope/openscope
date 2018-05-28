a39b35984838bbb6590834164a93ebf979880369
Erik Quinn <erikquinn2@gmail.com>
2018-05-27 22:29:20 -0500

    refactor/874 - Fix FMS docblock alignments

--- src/assets/scripts/client/aircraft/FlightManagementSystem/Fms.js
+++ src/assets/scripts/client/aircraft/FlightManagementSystem/Fms.js
@@ -83,12 +83,12 @@ export default class Fms {
         this.arrivalRunwayModel = null;
 
         /**
-        * Current flight phase of an aircraft
-        *
-        * @property currentPhase
-        * @type {string}
-        * @default ''
-        */
+         * Current flight phase of an aircraft
+         *
+         * @property currentPhase
+         * @type {string}
+         * @default ''
+         */
         this.currentPhase = '';
 
         /**
@@ -133,13 +133,13 @@ export default class Fms {
     }
 
     /**
-    * The active Leg in the `legCollection`
-    *
-    * Assumed to ALWAYS be the first `LegModel` in the `legCollection`
-    *
-    * @property currentLeg
-    * @type {LegModel}
-    */
+     * The active Leg in the `legCollection`
+     *
+     * Assumed to ALWAYS be the first `LegModel` in the `legCollection`
+     *
+     * @property currentLeg
+     * @type {LegModel}
+     */
     get currentLeg() {
         return this._routeModel.currentLeg;
     }
@@ -482,65 +482,65 @@ export default class Fms {
     }
 
     /**
-    * Return the next waypoint having an #altitudeMaximum equal to or less than the specified value
-    *
-    * This is helpful to see only future waypoints for which a particular altitude is
-    * considered NOT to be compliant. This allows us to focus which waypoints the aircraft
-    * will need to adjust altitude for, even if they are many waypoints in the future.
-    *
-    * @for Fms
-    * @method findNextWaypointWithMaximumAltitudeAtOrBelow
-    * @param altitude {number}
-    * @return {WaypointModel}
-    */
+     * Return the next waypoint having an #altitudeMaximum equal to or less than the specified value
+     *
+     * This is helpful to see only future waypoints for which a particular altitude is
+     * considered NOT to be compliant. This allows us to focus which waypoints the aircraft
+     * will need to adjust altitude for, even if they are many waypoints in the future.
+     *
+     * @for Fms
+     * @method findNextWaypointWithMaximumAltitudeAtOrBelow
+     * @param altitude {number}
+     * @return {WaypointModel}
+     */
     findNextWaypointWithMaximumAltitudeAtOrBelow(altitude) {
         return _find(this.waypoints, (waypointModel) => waypointModel.hasMaximumAltitudeAtOrBelow(altitude));
     }
 
     /**
-    * Return the next waypoint having a #speedMaximum equal to or less than the specified value
-    *
-    * This is helpful to see only future waypoints for which a particular speed is
-    * considered NOT to be compliant. This allows us to focus which waypoints the aircraft
-    * will need to adjust speed for, even if they are many waypoints in the future.
-    *
-    * @for Fms
-    * @method findNextWaypointWithMaximumSpeedAtOrBelow
-    * @param speed {number}
-    * @return {WaypointModel}
-    */
+     * Return the next waypoint having a #speedMaximum equal to or less than the specified value
+     *
+     * This is helpful to see only future waypoints for which a particular speed is
+     * considered NOT to be compliant. This allows us to focus which waypoints the aircraft
+     * will need to adjust speed for, even if they are many waypoints in the future.
+     *
+     * @for Fms
+     * @method findNextWaypointWithMaximumSpeedAtOrBelow
+     * @param speed {number}
+     * @return {WaypointModel}
+     */
     findNextWaypointWithMaximumSpeedAtOrBelow(speed) {
         return _find(this.waypoints, (waypointModel) => waypointModel.hasMaximumSpeedAtOrBelow(speed));
     }
 
     /**
-    * Return the next waypoint having an #altitudeMinimum equal to or greater than the specified value
-    *
-    * This is helpful to see only future waypoints for which a particular altitude is
-    * considered NOT to be compliant. This allows us to focus which waypoints the aircraft
-    * will need to adjust altitude for, even if they are many waypoints in the future.
-    *
-    * @for Fms
-    * @method findNextWaypointWithMinimumAltitudeAtOrAbove
-    * @param altitude {number}
-    * @return {WaypointModel}
-    */
+     * Return the next waypoint having an #altitudeMinimum equal to or greater than the specified value
+     *
+     * This is helpful to see only future waypoints for which a particular altitude is
+     * considered NOT to be compliant. This allows us to focus which waypoints the aircraft
+     * will need to adjust altitude for, even if they are many waypoints in the future.
+     *
+     * @for Fms
+     * @method findNextWaypointWithMinimumAltitudeAtOrAbove
+     * @param altitude {number}
+     * @return {WaypointModel}
+     */
     findNextWaypointWithMinimumAltitudeAtOrAbove(altitude) {
         return _find(this.waypoints, (waypointModel) => waypointModel.hasMinimumAltitudeAtOrAbove(altitude));
     }
 
     /**
-    * Return the next waypoint having a #speedMinimum equal to or greater than the specified value
-    *
-    * This is helpful to see only future waypoints for which a particular speed is
-    * considered NOT to be compliant. This allows us to focus which waypoints the aircraft
-    * will need to adjust speed for, even if they are many waypoints in the future.
-    *
-    * @for Fms
-    * @method findNextWaypointWithMinimumSpeedAtOrAbove
-    * @param speed {number}
-    * @return {WaypointModel}
-    */
+     * Return the next waypoint having a #speedMinimum equal to or greater than the specified value
+     *
+     * This is helpful to see only future waypoints for which a particular speed is
+     * considered NOT to be compliant. This allows us to focus which waypoints the aircraft
+     * will need to adjust speed for, even if they are many waypoints in the future.
+     *
+     * @for Fms
+     * @method findNextWaypointWithMinimumSpeedAtOrAbove
+     * @param speed {number}
+     * @return {WaypointModel}
+     */
     findNextWaypointWithMinimumSpeedAtOrAbove(speed) {
         return _find(this.waypoints, (waypointModel) => waypointModel.hasMinimumSpeedAtOrAbove(speed));
     }
@@ -557,48 +557,48 @@ export default class Fms {
     }
 
     /**
-    * Returns the lowest bottom altitude of any `LegModel` in the `#_legCollection`
-    *
-    * @for Fms
-    * @method getBottomAltitude
-    * @return {number}
-    */
+     * Returns the lowest bottom altitude of any `LegModel` in the `#_legCollection`
+     *
+     * @for Fms
+     * @method getBottomAltitude
+     * @return {number}
+     */
     getBottomAltitude() {
         return this._routeModel.getBottomAltitude();
     }
 
     /**
-    * Get the full flight plan route string (with airports removed), with legs separated by spaces
-    * For example, `KSEA16L.BANGR9.PANGL..TOU` --> `BANGR9 PANGL TOU`
-    *
-    * @for Fms
-    * @method getFullRouteStringWithoutAirportsWithSpaces
-    * @return {string}
-    */
+     * Get the full flight plan route string (with airports removed), with legs separated by spaces
+     * For example, `KSEA16L.BANGR9.PANGL..TOU` --> `BANGR9 PANGL TOU`
+     *
+     * @for Fms
+     * @method getFullRouteStringWithoutAirportsWithSpaces
+     * @return {string}
+     */
     getFullRouteStringWithoutAirportsWithSpaces() {
         return this._routeModel.getFullRouteStringWithoutAirportsWithSpaces();
     }
 
     /**
-    * Get the position of the next waypoint in the flight plan
-    *
-    * Currently only used in `calculateTurnInitiaionDistance()` helper function
-    *
-    * @for Fms
-    * @method getNextWaypointPositionModel
-    * @return waypointPosition {StaticPositionModel}
-    */
+     * Get the position of the next waypoint in the flight plan
+     *
+     * Currently only used in `calculateTurnInitiaionDistance()` helper function
+     *
+     * @for Fms
+     * @method getNextWaypointPositionModel
+     * @return waypointPosition {StaticPositionModel}
+     */
     getNextWaypointPositionModel() {
         return this.nextWaypoint.positionModel;
     }
 
     /**
-    * Return an array of waypoints in the flight plan that have altitude or speed restrictions
-    *
-    * @for Fms
-    * @method getRestrictedWaypoints
-    * @return {array<WaypointModel>}
-    */
+     * Return an array of waypoints in the flight plan that have altitude or speed restrictions
+     *
+     * @for Fms
+     * @method getRestrictedWaypoints
+     * @return {array<WaypointModel>}
+     */
     getRestrictedWaypoints() {
         return this.waypoints.filter((waypoint) => waypoint.hasRestriction);
     }
@@ -639,46 +639,46 @@ export default class Fms {
     }
 
     /**
-    * Returns the highest top altitude of any `LegModel` in the `#_legCollection`
-    *
-    * @for LegModel
-    * @method getTopAltitude
-    * @return {number}
-    */
+     * Returns the highest top altitude of any `LegModel` in the `#_legCollection`
+     *
+     * @for LegModel
+     * @method getTopAltitude
+     * @return {number}
+     */
     getTopAltitude() {
         return this._routeModel.getTopAltitude();
     }
 
     /**
-    * Return whether the route contains a waypoint after the currently active one
-    *
-    * @for Fms
-    * @method hasNextWaypoint
-    * @return {boolean}
-    */
+     * Return whether the route contains a waypoint after the currently active one
+     *
+     * @for Fms
+     * @method hasNextWaypoint
+     * @return {boolean}
+     */
     hasNextWaypoint() {
         return this._routeModel.hasNextWaypoint();
     }
 
     /**
-    * Return whether the route contains a waypoint with the specified name
-    *
-    * @for Fms
-    * @method hasWaypointName
-    * @param waypointName {string}
-    * @return {boolean}
-    */
+     * Return whether the route contains a waypoint with the specified name
+     *
+     * @for Fms
+     * @method hasWaypointName
+     * @param waypointName {string}
+     * @return {boolean}
+     */
     hasWaypointName(waypointName) {
         return this._routeModel.hasWaypointName(waypointName);
     }
 
     /**
-    *
-    *
-    * @for Fms
-    * @method isArrival
-    * @return {boolean}
-    */
+     *
+     *
+     * @for Fms
+     * @method isArrival
+     * @return {boolean}
+     */
     isArrival() {
         return this.currentPhase === FLIGHT_PHASE.CRUISE ||
         this.currentPhase === FLIGHT_PHASE.DESCENT ||
@@ -687,12 +687,12 @@ export default class Fms {
     }
 
     /**
-    *
-    *
-    * @for Fms
-    * @method isDeparture
-    * @return {boolean}
-    */
+     *
+     *
+     * @for Fms
+     * @method isDeparture
+     * @return {boolean}
+     */
     isDeparture() {
         return this.currentPhase === FLIGHT_PHASE.APRON ||
         this.currentPhase === FLIGHT_PHASE.TAXI ||
@@ -734,32 +734,32 @@ export default class Fms {
     }
 
     /**
-    * Move to the next possible waypoint
-    *
-    * This could be the next waypoint in the current leg,
-    * or the first waypoint in the next leg.
-    *
-    * @for LegModel
-    * @method moveToNextWaypoint
-    */
+     * Move to the next possible waypoint
+     *
+     * This could be the next waypoint in the current leg,
+     * or the first waypoint in the next leg.
+     *
+     * @for LegModel
+     * @method moveToNextWaypoint
+     */
     moveToNextWaypoint() {
         return this._routeModel.moveToNextWaypoint();
     }
 
     /**
-    * Find the arrival procedure (if it exists) within the `#legCollection` and
-    * reset it with a new arrival procedure.
-    *
-    * This method does not remove any `LegModel`s. It instead finds and updates a
-    * `LegModel` with a new routeString. If a `LegModel` without a arrival
-    * procedure cannot be found, then we create a new `LegModel` and place it
-    * at the end of the `#legCollection`.
-    *
-    * @for Fms
-    * @method replaceArrivalProcedure
-    * @param routeString {string}
-    * @return {array<boolean, string>}
-    */
+     * Find the arrival procedure (if it exists) within the `#legCollection` and
+     * reset it with a new arrival procedure.
+     *
+     * This method does not remove any `LegModel`s. It instead finds and updates a
+     * `LegModel` with a new routeString. If a `LegModel` without a arrival
+     * procedure cannot be found, then we create a new `LegModel` and place it
+     * at the end of the `#legCollection`.
+     *
+     * @for Fms
+     * @method replaceArrivalProcedure
+     * @param routeString {string}
+     * @return {array<boolean, string>}
+     */
     replaceArrivalProcedure(routeString) {
         const routeStringElements = routeString.split(PROCEDURE_OR_AIRWAY_SEGMENT_DIVIDER);
 
@@ -785,13 +785,13 @@ export default class Fms {
     }
 
     /**
-    * Replace departure procedure and departure runway
-    *
-    * @for Fms
-    * @method replaceDepartureProcedure
-    * @param routeString {string}
-    * @return {boolean}
-    */
+     * Replace departure procedure and departure runway
+     *
+     * @for Fms
+     * @method replaceDepartureProcedure
+     * @param routeString {string}
+     * @return {boolean}
+     */
     replaceDepartureProcedure(routeString) {
         const routeStringElements = routeString.split(PROCEDURE_OR_AIRWAY_SEGMENT_DIVIDER);
 
@@ -818,19 +818,19 @@ export default class Fms {
 
     // TODO: we may need to update the runway in this method
     /**
-    * Replace the current route with an entirely new one
-    *
-    * If route contains the same waypoint as the current waypoint, skip to that waypoint
-    * and continue along the new route. This is a somewhat questionable strategy, but
-    * has the advantage of supporting reroutes done in the middle of the flight,
-    * whereas without this approach, the aircraft would turn around to the very first
-    * waypoint as soon as they are rerouted.
-    *
-    * @for Fms
-    * @method replaceFlightPlanWithNewRoute
-    * @param routeString {string}
-    * @return {boolean} whether the operation was successful
-    */
+     * Replace the current route with an entirely new one
+     *
+     * If route contains the same waypoint as the current waypoint, skip to that waypoint
+     * and continue along the new route. This is a somewhat questionable strategy, but
+     * has the advantage of supporting reroutes done in the middle of the flight,
+     * whereas without this approach, the aircraft would turn around to the very first
+     * waypoint as soon as they are rerouted.
+     *
+     * @for Fms
+     * @method replaceFlightPlanWithNewRoute
+     * @param routeString {string}
+     * @return {boolean} whether the operation was successful
+     */
     replaceFlightPlanWithNewRoute(routeString) {
         const currentWaypointName = this.currentWaypoint.name;
         let nextRouteModel;
@@ -861,13 +861,13 @@ export default class Fms {
     }
 
     /**
-    * Verify and then set the value of `#arrivalRunwayModel`
-    *
-    * @for Fms
-    * @method setArrivalRunway
-    * @param nextRunwayModel {RunwayModel}
-    * @return {array} [success of operation, response]
-    */
+     * Verify and then set the value of `#arrivalRunwayModel`
+     *
+     * @for Fms
+     * @method setArrivalRunway
+     * @param nextRunwayModel {RunwayModel}
+     * @return {array} [success of operation, response]
+     */
     setArrivalRunway(nextRunwayModel) {
         const currentArrivalRunway = this.arrivalRunwayModel;
 
@@ -940,12 +940,12 @@ export default class Fms {
     }
 
     /**
-    * Fascade
-    *
-    * @for Fms
-    * @method skipToWaypointName
-    * @param waypointName {string}
-    */
+     * Fascade
+     *
+     * @for Fms
+     * @method skipToWaypointName
+     * @param waypointName {string}
+     */
     skipToWaypointName(waypointName) {
         return this._routeModel.skipToWaypointName(waypointName);
     }
