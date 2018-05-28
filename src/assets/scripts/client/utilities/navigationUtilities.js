abcce9e57e934ee83813b49e831e735ebdc69f8a
Erik Quinn <erikquinn2@gmail.com>
2018-05-27 22:34:29 -0500

    refactor/874 - Add docblock for navigation utility helper function

--- src/assets/scripts/client/utilities/navigationUtilities.js
+++ src/assets/scripts/client/utilities/navigationUtilities.js
@@ -1,5 +1,14 @@
 import { PROCEDURE_OR_AIRWAY_SEGMENT_DIVIDER } from '../constants/routeConstants';
 
+/**
+ * Combine entry, procedure/airway name, and exit to form a correctly formatted procedural route string
+ *
+ * @function assembleProceduralRouteString
+ * @param entryFix {string} ICAO identifier of fix where we enter the airway/procedure
+ * @param procedureOrAirway {string} ICAO identifier of the airway/procedure
+ * @param exitFix {string} ICAO identifier of fix where we exit the airway/procedure
+ * @return {string} correctly formatted procedural route string
+ */
 export function assembleProceduralRouteString(entryFix, procedureOrAirway, exitFix) {
     return `${entryFix}${PROCEDURE_OR_AIRWAY_SEGMENT_DIVIDER}${procedureOrAirway}` +
         `${PROCEDURE_OR_AIRWAY_SEGMENT_DIVIDER}${exitFix}`;
