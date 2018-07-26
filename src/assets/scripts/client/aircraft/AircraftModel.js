23ba6798cd9744dd47256a465805f9aab9bf3b50
Erik Quinn <erikquinn2@gmail.com>
2018-07-26 19:38:45 -0400

    bugfix/1034 - Fix waypoint time-to-turn calculations

--- src/assets/scripts/client/aircraft/AircraftModel.js
+++ src/assets/scripts/client/aircraft/AircraftModel.js
@@ -1757,7 +1757,8 @@ export default class AircraftModel {
         const waypointPosition = this.fms.currentWaypoint.positionModel;
         const distanceToWaypoint = this.positionModel.distanceToPosition(waypointPosition);
         const headingToWaypoint = this.positionModel.bearingToPosition(waypointPosition);
-        const isTimeToStartTurning = distanceToWaypoint < nm(calculateTurnInitiaionDistance(this, waypointPosition));
+        const timeToCompleteTurn = calculateTurnInitiaionDistance(this, waypointPosition);
+        const isTimeToStartTurning = distanceToWaypoint < timeToCompleteTurn;
         const closeToBeingOverFix = distanceToWaypoint < PERFORMANCE.MAXIMUM_DISTANCE_TO_PASS_WAYPOINT_NM;
         const closeEnoughToFlyByFix = distanceToWaypoint < PERFORMANCE.MAXIMUM_DISTANCE_TO_FLY_BY_WAYPOINT_NM;
         const shouldFlyByFix = closeEnoughToFlyByFix && isTimeToStartTurning;
--- test/math/flightMath.spec.js
+++ test/math/flightMath.spec.js
@@ -3,27 +3,27 @@ import ava from 'ava';
 
 import {
     calcTurnRadius,
-    calcTurnInitiationDistance,
+    calcTurnInitiationDistanceNm,
     bearingToPoint,
     fixRadialDist,
     calculateCrosswindAngle
 } from '../../src/assets/scripts/client/math/flightMath';
 
 ava('calcTurnRadius() returns a turn radius based on speed and bank angle', t => {
-    const speed = 144.0444432;
-    const bankAngle = 0.4363323129985824;
-    const expectedResult = 4535.774583027857;
+    const speed = 190;
+    const bankAngle = 0.523599;
+    const expectedResult = 0.9139236691657421;
     const result = calcTurnRadius(speed, bankAngle);
 
     t.true(result === expectedResult);
 });
 
 ava('calcTurnRadius() returns a turn radius based on speed and bank angle', t => {
-    const speed = 144.0444432;
-    const bankAngle = 0.4363323129985824;
+    const speed = 190;
+    const bankAngle = 0.523599;
     const courseChange = 0.26420086153126987;
-    const expectedResult = 746.732042830424;
-    const result = calcTurnInitiationDistance(speed, bankAngle, courseChange);
+    const expectedResult = 0.07685892074247777;
+    const result = calcTurnInitiationDistanceNm(speed, bankAngle, courseChange);
 
     t.true(result === expectedResult);
 });
