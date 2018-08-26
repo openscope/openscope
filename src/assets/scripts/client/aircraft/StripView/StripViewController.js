7f1d029d2fe34f03b21d304cc60227121072d45b
Felix Scheffer <mail@felix-scheffer.de>
2018-08-26 12:16:18 +0200

    bugfix/993 - addressed review issues

--- src/assets/scripts/client/aircraft/StripView/StripViewController.js
+++ src/assets/scripts/client/aircraft/StripView/StripViewController.js
@@ -147,6 +147,7 @@ export default class StripViewController {
             }
 
             let stripViewModel = this._collection.findStripByAircraftId(aircraftModel.id);
+            
             if (typeof stripViewModel === "undefined") {
                 stripViewModel = this.createStripView(aircraftModel);
             }
