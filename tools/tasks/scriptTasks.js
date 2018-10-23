0dc963ebad3115e941ff3fa2645558fab99c4562
Nate Geslin <teamtomkins23@gmail.com>
2018-10-22 21:21:36 -0500

    devops/1137 - removes nodemon * updates start script to use vanilla node * updates server.js to use require instead of import * removes gulp-babel * updates gulp build:server to use a simple gulp copy

--- tools/tasks/scriptTasks.js
+++ tools/tasks/scriptTasks.js
@@ -2,13 +2,11 @@
 
 module.exports = function(gulp, config) {
     const OPTIONS = config;
-
     const cli = require('../cli');
 
     function buildScripts() {
         const browserify = require('browserify');
         const babelify = require('babelify');
-        const babel = require('gulp-babel');
         const uglify = require('gulp-uglify');
         const gulpif = require('gulp-if');
         const sourcemaps = require('gulp-sourcemaps');
@@ -42,63 +40,17 @@ module.exports = function(gulp, config) {
     gulp.task('babel', () => buildScripts());
 
     gulp.task('babel-server', () => {
-        const babel = require('gulp-babel');
         const path = require('path');
 
         return gulp.src([path.join(OPTIONS.DIR.SRC_SCRIPTS_SERVER, '**/*.js')])
-            .pipe(babel({
-                presets: ['es2015']
-            }))
-            .pipe(gulp.dest(OPTIONS.DIR.DIST_SCRIPTS_SERVER))
+            .pipe(gulp.dest(OPTIONS.DIR.DIST_SCRIPTS_SERVER));
     });
-
-    ////////////////////////////////////////////////////////////////////
-    // ESLINT
-    ////////////////////////////////////////////////////////////////////
-    // const gutil = require('gulp-util');
-    // const eslint = require('gulp-eslint');
-    // const Table = require('cli-table');
-    // const t = new Table({
-    //     head: ['Filename', 'Errors', 'Wranings']
-    // });
-    //
-    // gulp.task('lint:scripts', function() {
-    //     gulp.src([OPTIONS.GLOB.JS])
-    //         .pipe(eslint({
-    //             useEslintrc: true
-    //         }))
-    //         .pipe(eslint.result((result) => {
-    //             t.push([
-    //                 result.filePath.split('scripts')[1],
-    //                 gutil.colors.red(result.errorCount),
-    //                 gutil.colors.yellow(result.warningCount)
-    //             ]);
-    //         }))
-    //         .pipe(eslint.results(results => {
-    //             // Add a footer to the results table
-    //             t.push([
-    //                 gutil.colors.cyan(`Total: ${results.length}`),
-    //                 gutil.colors.red(results.errorCount),
-    //                 gutil.colors.yellow(results.warningCount)
-    //             ])
-    //
-    //             gutil.log(gutil.colors.cyan('--- ---- --- ESLint Results --- ---- --- '));
-    //             console.log('\n' + t);
-    //             // // Called once for all ESLint results.
-    //             // gutil.log('\n');
-    //             // gutil.log(gutil.colors.cyan(`Total Files: ${results.length}`));
-    //             // gutil.log(gutil.colors.yellow(`Total Warnings: ${results.warningCount}`));
-    //             // gutil.log(gutil.colors.red(`Total Errors: ${results.errorCount}`));
-    //         }))
-    //         .pipe(eslint.format('checkstyle'));
-    // });
 
     ////////////////////////////////////////////////////////////////////
     // TASKS
     ////////////////////////////////////////////////////////////////////
     gulp.task('build:scripts', ['babel']);
     gulp.task('build:server', ['babel-server']);
-
     gulp.task('watch:scripts', () => {
         gulp.watch(OPTIONS.GLOB.JS, ['babel-server', 'babel']);
     });
