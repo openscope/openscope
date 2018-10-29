fe721a554d47b6b282ff983f0b7bf8c6836e532d
Erik Quinn <erikquinn2@gmail.com>
2018-10-29 14:46:42 -0400

    bugfix/955 - Change routestring to uppercase in FMS, not in a/c commander

--- src/assets/scripts/client/aircraft/AircraftCommander.js
+++ src/assets/scripts/client/aircraft/AircraftCommander.js
@@ -373,7 +373,7 @@ export default class AircraftCommander {
      * @return {array}   [success of operation, readback]
      */
     runSID(aircraft, data) {
-        const routeString = data[0].toUpperCase();
+        const routeString = data[0];
         const airportModel = AirportController.airport_get();
 
         return aircraft.pilot.applyDepartureProcedure(routeString, airportModel.icao);
@@ -386,7 +386,7 @@ export default class AircraftCommander {
      * @return {array}   [success of operation, readback]
      */
     runSTAR(aircraft, data) {
-        const routeString = data[0].toUpperCase();
+        const routeString = data[0];
         const airportModel = AirportController.airport_get();
 
         return aircraft.pilot.applyArrivalProcedure(routeString, airportModel.name);
