8ead8ee4a0b04eb6aae2ddb7e9e7d40074490256
Felix Scheffer <mail@felix-scheffer.de>
2018-10-29 01:59:05 +0100

    feature/1003 - another bug fix

--- src/assets/scripts/client/aircraft/StripView/StripViewModel.js
+++ src/assets/scripts/client/aircraft/StripView/StripViewModel.js
@@ -665,7 +665,7 @@ export default class StripViewModel extends BaseModel {
     _buildRunwayInformation(aircraftModel) {
         if (aircraftModel.isDeparture() && aircraftModel.isOnGround()) {
             return {
-                assigned: aircraftModel.isApron(),
+                assigned: !aircraftModel.isApron(),
                 name: aircraftModel.fms.departureRunwayModel.name
             };
         }
