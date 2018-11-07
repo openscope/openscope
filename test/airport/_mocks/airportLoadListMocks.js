51f6047101ddb2566716d978e89e8de55f58b25e
JPmAn24 <joelarndt24@gmail.com>
2018-11-07 10:48:35 -0600

    refactor/1001 - Addredd review comments

--- test/airport/_mocks/airportLoadListMocks.js
+++ test/airport/_mocks/airportLoadListMocks.js
@@ -3,18 +3,18 @@ export const AIRPORT_LOAD_LIST_MOCK = [
         icao: 'klas',
         level: 'medium',
         name: 'McCarran International Airport',
-        premium: false,
+        premium: false
     },
     {
         icao: 'ksea',
         level: 'medium',
         name: 'Seattle-Tacoma International Airport',
-        premium: false,
+        premium: false
     },
     {
         icao: 'ksfo',
         level: 'medium',
         name: 'San Francisco International Airport &#9983',
-        premium: false,
+        premium: false
     }
 ];
