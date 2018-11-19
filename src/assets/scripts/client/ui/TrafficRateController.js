dd7ee7bec27e737244d36ba5f7d049e84b4c2013
Felix Scheffer <mail@felix-scheffer.de>
2018-11-19 02:19:25 +0100

    feature/16 - rebuilds traffic rate dialog when airport changes

--- src/assets/scripts/client/ui/TrafficRateController.js
+++ src/assets/scripts/client/ui/TrafficRateController.js
@@ -1,8 +1,10 @@
 import $ from 'jquery';
 import _forEach from 'lodash/forEach';
 import SpawnPatternCollection from '../trafficGenerator/SpawnPatternCollection';
+import EventBus from '../lib/EventBus';
 import { SELECTORS } from '../constants/selectors';
 import { FLIGHT_CATEGORY } from '../constants/aircraftConstants';
+import { EVENT } from '../constants/eventNames';
 
 /**
  * @property UI_SETTINGS_MODAL_TEMPLATE
@@ -17,6 +19,14 @@ const UI_TRAFFIC_MODAL_TEMPLATE = '<div class="traffic-dialog dialog"><p class="
 export default class TrafficRateController {
 
     constructor($element) {
+        /**
+         * @property EventBus
+         * @type {EventBus}
+         * @default EventBus
+         * @private
+         */
+        this._eventBus = EventBus;
+
         /**
          * Root DOM element
          *
@@ -35,18 +45,107 @@ export default class TrafficRateController {
          */
         this.$dialog = null;
 
-        this.init();
+        this.init()
+            ._setupHandlers()
+            .enable();
     }
 
     /**
      *
-     * @for SettingsController
+     * @for TrafficRateController
      * @method init
      * @chainable
      */
     init() {
         this.$dialog = $(UI_TRAFFIC_MODAL_TEMPLATE);
         this.$body = this.$dialog.find('.dialog-body');
+
+        this._buildDialogBody();
+        this.$element.append(this.$dialog);
+
+        return this;
+    }
+
+    /**
+     * Create event handlers
+     *
+     * Should be run once only on instantiation
+     *
+     * @for TrafficRateController
+     * @method _setupHandlers
+     * @chainable
+     */
+    _setupHandlers() {
+        this._onAirportChangeHandler = this.onAirportChange.bind(this);
+
+        return this;
+    }
+
+    /**
+     * Enable event handlers
+     *
+     * should be run only once on instantiation
+     *
+     * @for TrafficRateController
+     * @method enable
+     * @chainable
+     */
+    enable() {
+        this._eventBus.on(EVENT.AIRPORT_CHANGE, this._onAirportChangeHandler);
+
+        return this;
+    }
+
+    /**
+     * Disable event handlers
+     *
+     * @for TrafficRateController
+     * @method disable
+     * @chainable
+     */
+    disable() {
+        this._eventBus.off(EVENT.AIRPORT_CHANGE, this._onAirportChangeHandler);
+
+        return this;
+    }
+
+    /**
+     * Returns whether the airport selection dialog is open
+     *
+     * @for TrafficRateController
+     * @method isDialogOpen
+     * @return {boolean}
+     */
+    isDialogOpen() {
+        return this.$dialog.hasClass(SELECTORS.CLASSNAMES.OPEN);
+    }
+
+    /**
+    * @for TrafficRateController
+    * @method toggleDialog
+    */
+    toggleDialog() {
+        this.$dialog.toggleClass(SELECTORS.CLASSNAMES.OPEN);
+    }
+
+    /**
+     * Rebuilds the dialog body when the airport is changed.
+     *
+     * @for TrafficRateController
+     * @method onAirportChange
+     */
+    onAirportChange() {
+        this._buildDialogBody();
+    }
+
+    /**
+     * Builds the dialog body
+     *
+     * @for TrafficRateController
+     * @method _buildDialogBody
+     */
+    _buildDialogBody() {
+        this.$body.empty();
 
         _forEach(['departures', 'arrivals', 'overflights'], (opt) => {
             const $option = `<div class="option">
@@ -95,28 +194,5 @@ export default class TrafficRateController {
                             </div>`;
             this.$body.append($option);
         });
-
-        this.$element.append(this.$dialog);
-
-        return this;
-    }
-
-    /**
-     * Returns whether the airport selection dialog is open
-     *
-     * @for TrafficRateController
-     * @method isDialogOpen
-     * @return {boolean}
-     */
-    isDialogOpen() {
-        return this.$dialog.hasClass(SELECTORS.CLASSNAMES.OPEN);
-    }
-
-    /**
-    * @for TrafficRateController
-    * @method toggleDialog
-    */
-    toggleDialog() {
-        this.$dialog.toggleClass(SELECTORS.CLASSNAMES.OPEN);
     }
 }
