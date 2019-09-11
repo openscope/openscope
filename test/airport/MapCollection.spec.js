65a5b3dca769fefa41a6baef9bf11a50ec479e04
John Bayly <3430117+oobayly@users.noreply.github.com>
2019-09-11 17:43:53 +0100

    enhancement/1436 - Add helper property and method tests

--- test/airport/MapCollection.spec.js
+++ test/airport/MapCollection.spec.js
@@ -25,11 +25,15 @@ ava('accepts a map array that is used to set the instance properties', t => {
 
   t.not(typeof model._id, 'undefined');
   t.is(model.length, MAP_MOCK.length);
+  t.is(model.getMapNames().length, model.length);
+  t.true(model.hasMaps);
+  t.true(model.hasVisibleMaps);
 
   const first = model.maps[0];
 
   t.is(first.name, MAP_MOCK[0].name);
   t.is(first.lines.length, MAP_MOCK[0].lines.length);
+  t.true(first.hasLines);
 });
 
 ava('accepts a legacy map object that is used to set the instance properties', t => {
@@ -37,9 +41,13 @@ ava('accepts a legacy map object that is used to set the instance properties', t
 
   t.not(typeof model._id, 'undefined');
   t.is(model.length, Object.keys(MAP_MOCK_LEGACY).length);
+  t.is(model.getMapNames().length, model.length);
+  t.true(model.hasMaps);
+  t.true(model.hasVisibleMaps);
 
   const first = model.maps[0];
 
   t.is(first.name, `Legacy-${Object.keys(MAP_MOCK_LEGACY)[0]}`);
   t.is(first.lines.length, MAP_MOCK_LEGACY.base.length);
+  t.true(first.hasLines);
 });
