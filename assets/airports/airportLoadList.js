1853374275f12f23adc92cdfaf9b041044579b0d
Fratee <43710005+Fratee@users.noreply.github.com>
2020-02-01 17:42:35 +0100

    Fixed tab

--- assets/airports/airportLoadList.js
+++ assets/airports/airportLoadList.js
@@ -366,7 +366,7 @@ window.AIRPORT_LOAD_LIST = (function() { // eslint-disable-line wrap-iife
         //     name: 'Tucson International Airport',
         //     premium: false
         // },
-		{
+        {
             icao: 'lipz',
             level: 'medium',
             name: 'Venezia-Marco Polo International Airport',
