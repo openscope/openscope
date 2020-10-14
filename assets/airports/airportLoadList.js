01f383654fa78bdd34f43d04338ff5bdc9e747dc
Michael McCracken <michael@Michaels-MacBook-Pro.local>
2020-10-14 19:33:05 -0400

    enhancement/1654 - Add KGSO airport, terrain, and airport load list

--- assets/airports/airportLoadList.js
+++ assets/airports/airportLoadList.js
@@ -217,6 +217,12 @@ window.AIRPORT_LOAD_LIST = (function() { // eslint-disable-line wrap-iife
             premium: false
         },
         {
+            icao: 'kdab',
+            level: 'beginner',
+            name: 'Daytona Beach International Airport',
+            premium: false
+        },
+        {
             icao: 'kdca',
             level: 'easy',
             name: 'Ronald Reagan Washington National Airport',
