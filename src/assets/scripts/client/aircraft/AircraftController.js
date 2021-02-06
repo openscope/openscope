751063917d407bead2440962338401991626bb49
cake-pie <cake-pie@users.noreply.github.com>
2021-02-06 15:29:46 +0800

    devops/1749 - AircraftController

--- src/assets/scripts/client/aircraft/AircraftController.js
+++ src/assets/scripts/client/aircraft/AircraftController.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-continue */
 import _find from 'lodash/find';
 import _get from 'lodash/get';
-import _isObject from 'lodash/isObject';
+import _isNil from 'lodash/isNil';
 import _without from 'lodash/without';
+import AirlineController from '../airline/AirlineController';
 import AirportController from '../airport/AirportController';
+import ScopeModel from '../scope/ScopeModel';
 import UiController from '../ui/UiController';
 import EventBus from '../lib/EventBus';
 import AircraftTypeDefinitionCollection from './AircraftTypeDefinitionCollection';
@@ -40,15 +42,25 @@ export default class AircraftController {
      * @param scopeModel {ScopeModel}
      */
     constructor(aircraftTypeDefinitionList, airlineController, scopeModel) {
-        if (isEmptyOrNotArray(aircraftTypeDefinitionList)) {
-            // eslint-disable-next-line max-len
-            throw new TypeError('Invalid aircraftTypeDefinitionList passed to AircraftTypeDefinitionCollection. ' +
-                `Expected and array but received ${typeof aircraftTypeDefinitionList}`);
+        if (_isNil(aircraftTypeDefinitionList) || _isNil(airlineController) || _isNil(scopeModel)) {
+            throw new TypeError('Invalid parameter(s) passed to AircraftController constructor. ' +
+                'Expected aircraftTypeDefinitionList, airlineController and scopeModel to be defined, ' +
+                `but received ${typeof aircraftTypeDefinitionList}, ${typeof airlineController} and ${typeof scopeModel}`);
         }
 
-        // TODO: this may need to use instanceof instead, but that may be overly defensive
-        if (!_isObject(airlineController)) {
-            throw new TypeError('Invalid parameters. Expected airlineCollection to be defined');
+        if (isEmptyOrNotArray(aircraftTypeDefinitionList)) {
+            throw new TypeError('Invalid aircraftTypeDefinitionList passed to AircraftController constructor. ' +
+                `Expected a non-empty array, but received ${typeof aircraftTypeDefinitionList}`);
+        }
+
+        if (!(airlineController instanceof AirlineController)) {
+            throw new TypeError('Invalid airlineController passed to AircraftController constructor. ' +
+                `Expected instance of AirlineController, but received ${typeof airlineController}`);
+        }
+
+        if (!(scopeModel instanceof ScopeModel)) {
+            throw new TypeError('Invalid scopeModel passed to AircraftController constructor. ' +
+                `Expected instance of ScopeModel, but received ${typeof scopeModel}`);
         }
 
         /**
