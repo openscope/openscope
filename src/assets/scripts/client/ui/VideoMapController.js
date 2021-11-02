d3b64e2fa5f695596b66daf7bbde831d25b818dd
CanisLupus518 <habelson@howlerinteractive.com>
2021-11-02 17:33:00 -0400

    feature/1848 - Additional lint cleanup

--- src/assets/scripts/client/ui/VideoMapController.js
+++ src/assets/scripts/client/ui/VideoMapController.js
@@ -166,7 +166,6 @@ export default class VideoMapController {
             const $formElement = this._buildRow(mapName, mapName, isChecked, this._onChangeSelectedMaps);
             this.$dialogBody.append($formElement);
         }
-
     }
 
     /**
