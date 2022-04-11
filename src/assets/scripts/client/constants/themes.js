2b14b9c0b804ee6b4481b95ab33cf5c842782c73
CanisLupus518 <habelson@howlerinteractive.com>
2022-04-11 15:07:46 -0400

    feature/1938 - create STARS-like theme called 'Celestial'

--- src/assets/scripts/client/constants/themes.js
+++ src/assets/scripts/client/constants/themes.js
@@ -1,7 +1,9 @@
 import { THEME_CLASSIC } from './themes/classic/theme';
 import { THEME_DEFAULT } from './themes/default/theme';
+import { THEME_CELESTIAL } from './themes/celestial/theme';
 
 export const THEME = {
     CLASSIC: THEME_CLASSIC,
-    DEFAULT: THEME_DEFAULT
+    DEFAULT: THEME_DEFAULT,
+    CELESTIAL: THEME_CELESTIAL
 };
